e12305eab4d3228abfedbbf1e3bfe8e92f9eb7a4
Michael Bamford <michael@michaelbamford.com>
2010-03-13 14:27:32 -0500

    Layout switched to 960gs.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,25 +1,17 @@
 class User < ActiveRecord::Base
   
-  # EmailPattern = /(\S+)@(\S+)/
-
   include AASM
   
   aasm_column        :status
   aasm_initial_state :welcomed
   aasm_state         :welcomed
   
-  # validates_format_of :email, :with => EmailPattern
-  
-  acts_as_authentic #do |config|
-    # config.validate_login_field = false
-    # config.validate_email_field = true
-    # config.logged_in_timeout    = 30.minutes
-    # config.validate_password_field = false
-    # config.require_password_confirmation = false
-  #end
+  acts_as_authentic do |config|
+    config.logged_in_timeout = 30.minutes
+  end
 
-  # def require_password?
-  #   false
-  # end
+  def require_password?
+    true
+  end
   
 end
