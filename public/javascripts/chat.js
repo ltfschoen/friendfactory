1b1690aabe66a144af294645271db284211d4c41
Michael Bamford <michael@michaelbamford.com>
2010-08-25 12:13:24 -0400

    Upgraded to pusher 1.6

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -1,18 +1,16 @@
 jQuery(function($){
-	$('form.new_posting_chat').submit(function(event) {
-    event.preventDefault();
+	$('form.new_posting_chat').live('submit', function(event) {
+	  event.preventDefault();
 		$this = $(this);
     if ($this.find('input[type=text]').val().length > 0) {
 			var action = $this.attr('action');
       $.ajax({
-        data: $.param($this.serializeArray()),
+        data: jQuery.param(jQuery(this).serializeArray()),
         dataType: 'script',
         type: 'post',
         url: action,
         success: function(response, status) {
-					if (status == 'success') {
-						$this.reset();
-					}
+					if (status == 'success') { $this.reset(); }
 				}
 			});
 		}
