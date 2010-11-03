558fe584ddd4a7b8173bb4788423b24e6ff1a12d
Michael Bamford <michael@michaelbamford.com>
2010-11-02 20:26:04 -0700

    Added attachment reprocess to migration

--- db/migrate/20100823042929_create_posting_chats.rb
+++ db/migrate/20100823042929_create_posting_chats.rb
@@ -1,10 +1,12 @@
 class CreatePostingChats < ActiveRecord::Migration
   def self.up
-    create_table :posting_chats do |t|
-      t.integer   :receiver_id
-      t.text      :body
+    unless table_exists?(:posting_chats)
+      create_table :posting_chats do |t|
+        t.integer   :receiver_id
+        t.text      :body
+      end
+      add_index :posting_chats, :receiver_id
     end
-    add_index :posting_chats, :receiver_id
   end
 
   def self.down
--- db/migrate/20101101042055_add_dimensions_to_posting.rb
+++ db/migrate/20101101042055_add_dimensions_to_posting.rb
@@ -1,13 +1,33 @@
+require 'rake'
+
 class AddDimensionsToPosting < ActiveRecord::Migration
-  def self.up
+  def self.up  
     add_column :postings, :width, :integer
     add_column :postings, :height, :integer
     add_column :postings, :horizontal, :boolean
+    
+    puts '-- ff:attachments:reprocess!'
+    Rake::Task[:'ff:attachments:reprocess!'].invoke
+    
+    puts '-- ff:attachments:geometry!'
+    Rake::Task[:'ff:attachments:geometry!'].invoke    
+
+    puts '-- update default wave slug'
+    update_default_wave('shared', ::WavesController::DefaultWaveSlug)
   end
 
   def self.down
     remove_column :postings, :width rescue nil
     remove_column :postings, :height rescue nil
-    remove_column :postings, :horizontal rescue nil    
+    remove_column :postings, :horizontal rescue nil
+    update_default_wave(::WavesController::DefaultWaveSlug, 'shared')
   end
+end
+
+def update_default_wave(old_slug, new_slug)
+  wave = Wave::Base.find_by_slug(old_slug)
+  if wave.present?
+    wave.slug = new_slug
+    wave.save
+  end  
 end
