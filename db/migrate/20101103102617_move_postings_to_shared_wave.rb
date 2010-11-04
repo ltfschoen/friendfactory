c7e28a08a5bd94f2605fa526e0238a49fd4eb262
Michael Bamford <michael@michaelbamford.com>
2010-11-03 18:04:13 -0700

    Removed hard-coded IDs from migration

--- db/migrate/20101103102617_move_postings_to_shared_wave.rb
+++ db/migrate/20101103102617_move_postings_to_shared_wave.rb
@@ -3,20 +3,19 @@ class MovePostingsToSharedWave < ActiveRecord::Migration
     say 'ff:db:default_wave'
     Rake::Task[:'ff:db:default_wave'].invoke
 
-    say 'moving postings from profile wave (id=2) to shared wave (id=1)'
     user = User.find_by_email('michael@michaelbamford.com')
     shared_wave = Wave::Base.find_by_slug(WavesController::DefaultWaveSlug)
+    say "moving postings from profile wave (id=#{user.profile.id}) to shared wave (id=#{shared_wave.id})"
     
     if user.present? && shared_wave.present?
       profile_posting_ids = user.profile.posting_ids - user.profile.avatar_ids
       shared_wave.posting_ids = (shared_wave.posting_ids + profile_posting_ids).uniq
       user.profile.posting_ids = (user.profile.posting_ids - profile_posting_ids)
-
       say "moved #{profile_posting_ids.sort * ' '}", true
       say "kept  #{user.profile.posting_ids.sort * ' '}", true
     end
     
-    say 'copying active avatars from all profile waves to shared wave (id=1)'
+    say "copying active avatars from all profile waves to shared wave (id=#{shared_wave.id})"
     avatar_ids = Wave::Profile.all.map{ |profile| profile.avatar_id }.compact
     copied_ids = (shared_wave.posting_ids + avatar_ids).uniq - shared_wave.posting_ids
     shared_wave.posting_ids = (shared_wave.posting_ids + avatar_ids).uniq
