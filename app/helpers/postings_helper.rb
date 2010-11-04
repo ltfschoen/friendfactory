f4029e4c434df62a517baba34fe73fd4726cc7bb
Michael Bamford <michael@michaelbamford.com>
2010-11-03 22:58:42 -0700

    Moved postings view to posting namespace

--- app/helpers/postings_helper.rb
+++ app/helpers/postings_helper.rb
@@ -2,4 +2,8 @@ module PostingsHelper
   def render_post_it(object)
     render(:partial => '/posting/post_it', :locals => { :posting => object })
   end
+  
+  def render_postings(collection)
+    render :partial => 'posting/posting', :collection => collection
+  end
 end
