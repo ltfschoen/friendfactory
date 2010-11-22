a1eef8701041087608e665a7a1b7e1b75e0d1f64
Michael Bamford <michael@michaelbamford.com>
2010-11-21 22:55:42 -0700

    Fragment cache of polariods in memcached on production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,14 +25,14 @@ Friskyfactory::Application.configure do
   # config.logger = SyslogLogger.new
 
   # Use a different cache store in production
-  # config.cache_store = :mem_cache_store
+  config.cache_store = :mem_cache_store, 'localhost'
 
   # Disable Rails's static asset server
   # In production, Apache or nginx will already do this
   config.serve_static_assets = false
 
   # Enable serving of images, stylesheets, and javascripts from an asset server
-  # config.action_controller.asset_host = "http://assets.example.com"
+  config.action_controller.asset_host = "http://assets%d.friskyhands.com"
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
