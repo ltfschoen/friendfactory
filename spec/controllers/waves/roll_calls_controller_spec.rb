7b32cea2ee24f35a60434049a62e1b544f352bf7
Michael Bamford <michael@michaelbamford.com>
2010-11-28 02:43:06 -0700

    Removed obsolete polaroid wave (now roll call wave)

--- spec/controllers/waves/roll_calls_controller_spec.rb
+++ spec/controllers/waves/roll_calls_controller_spec.rb
@@ -1,15 +1,16 @@
 require 'spec_helper'
 
-describe Waves::PolaroidsController do
+describe Waves::RollCallsController do
 
   def mock_wave(stubs={})
-    (@mock_wave ||= mock_model(Wave::Polaroid).as_null_object).tap do |wave|
+    (@mock_wave ||= mock_model(Wave::RollCall).as_null_object).tap do |wave|
       wave.stub(stubs) unless stubs.empty?
     end
   end
 
   describe "GET index" do
     it "assigns polaroid dynamic wave to @wave" do
+      pending
       posting = Factory.build(:avatar)
       wave = double('polaroid_wave')
       Struct::PolaroidWave.should_receive(:new).and_return(wave)
