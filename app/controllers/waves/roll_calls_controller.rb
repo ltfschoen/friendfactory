0932e8b6ac795ee48ccbb9655cc2ea07b614ec19
Michael Bamford <michael@michaelbamford.com>
2010-11-28 02:39:38 -0700

    Simplied roll call. Removed dynamic struct wave

--- app/controllers/waves/roll_calls_controller.rb
+++ app/controllers/waves/roll_calls_controller.rb
@@ -1,18 +1,14 @@
 class Waves::RollCallsController < Waves::BaseController
 
-  RollCallWave   = Struct.new('RollCallWave', :id, :topic, :profiles)
-  RollCallWaveID = '000'
-
   before_filter :require_lurker
 
   def index
-    profiles = if params[:tag]
+    @waves = if params[:tag]
       params[:tag] = params[:tag].downcase.gsub(/-/, ' ')
       UserInfo.tagged_with(params[:tag]).map(&:profile)
     else
       Wave::Profile.includes(:resource, :avatars).order('updated_at desc')
     end
-    @wave = RollCallWave.new(RollCallWaveID, 'Roll Call', profiles)
     @tags = UserInfo.tag_counts_on(:tags).order('name asc')
     respond_to do |format|
       format.html
