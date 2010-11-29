ad537c62eb16d9d4d472c77e11d5c21c2dc3af58
Michael Bamford <michael@michaelbamford.com>
2010-11-28 22:27:58 -0700

    Moved some posting specs to correct namespace location

---
+++ spec/models/posting/photo_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+
+describe Posting::Photo do
+  before(:each) do
+    @valid_attributes = {
+      
+    }
+  end
+
+  it "should create a new instance given valid attributes" do
+    pending
+    Photo.create!(@valid_attributes)
+  end
+end
---
+++ spec/models/wave/profile_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+
+describe Wave::Profile do
+  before(:each) do
+    @valid_attributes = {
+      
+    }
+  end
+
+  it "should create a new instance given valid attributes" do
+    pending
+    Profile.create!(@valid_attributes)
+  end
+end
