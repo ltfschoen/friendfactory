91d68ae9ff5e9137d4d5c9b0fe59766e17a90ddc
Michael Bamford <michael@michaelbamford.com>
2010-12-03 03:26:11 -0700

    Removed empty helpers

--- app/helpers/chats_helper.rb
+++ app/helpers/chats_helper.rb
@@ -1,6 +1,6 @@
 require 'digest/sha1'
 
-module Posting::ChatsHelper
+module ChatsHelper
   def chat_channel(sender, receiver)
     Digest::SHA1.hexdigest([ sender, receiver ].sort.to_s)[0..7]
   end
--- app/helpers/wave/events_helper.rb
+++
@@ -1,2 +0,0 @@
-module Wave::EventsHelper
-end
