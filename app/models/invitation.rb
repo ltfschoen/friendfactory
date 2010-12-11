9c7e2fb1e2bea5fd37623c910569aa1f99a06958
Michael Bamford <michael@michaelbamford.com>
2010-12-10 21:19:18 -0700

    Disable invitation model

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -1,11 +1,11 @@
 class Invitation < ActiveRecord::Base
   
-  has_many :events,
-      :class_name  => 'Wave::Event',
-      :foreign_key => 'event_id'
+  # has_many :events,
+  #     :class_name  => 'Wave::Event',
+  #     :foreign_key => 'event_id'
   
-  has_many :profiles,
-      :class_name  => 'Wave::Profile',
-      :foriegn_key => 'profile_id'
+  # has_many :profiles,
+  #     :class_name  => 'Wave::Profile',
+  #     :foreign_key => 'profile_id'
 
 end
