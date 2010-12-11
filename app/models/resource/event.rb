0d57393fd7f5d56f7d9c04984e72a458817c3918
Michael Bamford <michael@michaelbamford.com>
2010-12-10 21:30:06 -0700

    Fixed bug with nil event dates

--- app/models/resource/event.rb
+++ app/models/resource/event.rb
@@ -2,7 +2,8 @@ module Resource
   class Event < ActiveRecord::Base
     has_one :event, :as => :resource, :class_name => 'Wave::Event'    
     def start_date=(date)
-      write_attribute(:start_date, Date.strptime(date, '%m/%d/%Y'))
+      formatted_date = Date.strptime(date, '%m/%d/%Y') rescue nil
+      write_attribute(:start_date, formatted_date) 
     end
   end
 end
