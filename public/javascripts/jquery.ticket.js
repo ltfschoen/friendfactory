6c13aa515381f3ad989a70bb02b5dd00662f03e7
Michael Bamford <michael@michaelbamford.com>
2010-12-19 22:44:04 -0700

    Flip ticket for non-safari browsers

--- public/javascripts/jquery.ticket.js
+++ public/javascripts/jquery.ticket.js
@@ -8,8 +8,38 @@
 				$this.click(function(event) {
 					$(this).closest('.ticket').toggleClass('flipped');
 				});				
-			});			
-		}// if
+			});
+			
+		} else {			
+
+			return this.each(function() {
+				
+				$this = $(this); // ticket
+				$this.find('.back.face').css('-webkit-transform', 'rotateY(0deg)');
+				$this.find('.face-container:eq(1)').hide();
+				
+				$this.find('.front.face').live('click', function(event) {
+					event.preventDefault();					
+					var $ticket = $(this).closest('.ticket');
+					
+					$ticket.flip({
+						speed: 300,
+						direction: 'lr',
+						dontChangeColor: true,
+						content: $ticket.find('.face-container:hidden'),
+						
+						onEnd: function() {
+							$ticket.find('.back.face').click(function(event) {
+								event.preventDefault();
+								$ticket.revertFlip();
+							}); // click
+						} // onEnd						
+					}); // flip
+					
+				}); // click
+			}) //each
+			
+		} // if
 		
 	} // fn.ticket
 	
