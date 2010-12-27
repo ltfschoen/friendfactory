028dffbdf8c19f8b6c12ea44e10c7f1f0d048ca6
Michael Bamford <michael@michaelbamford.com>
2010-12-27 11:59:16 -0700

    Removed delivery of emails on staging

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -47,7 +47,7 @@ Friskyfactory::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
-  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.delivery_method = :test # :smtp
   config.action_mailer.smtp_settings = {
       :address              => "smtp.gmail.com",
       :port                 => 587,
