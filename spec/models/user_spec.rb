e7497ad8bf097c7bba3a95dbce35296a9976229e
Michael Bamford <michael@michaelbamford.com>
2011-01-08 22:55:37 -0700

    Specs for admin

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,43 +2,61 @@ require 'spec_helper'
 
 describe User do
   
-  let(:attrs) do
-    { :first_name => 'duncan',
-      :email => 'duncan@test.com',
-      :password => 'test',
-      :password_confirmation => 'test' }
-  end
+  fixtures :users
+  
+  describe 'attributes' do
+    let(:attrs) do
+      { :first_name => 'duncan',
+        :email => 'duncan@test.com',
+        :password => 'test',
+        :password_confirmation => 'test' }
+    end
 
-  let(:user) do
-    User.new(attrs)
-  end
+    let(:user) do
+      User.new(attrs)
+    end
   
-  it "is valid with valid attributes" do
-    user.should be_valid
-  end
+    it "is valid with valid attributes" do
+      user.should be_valid
+    end
   
-  it 'requires a first_name' do
-    user.first_name = nil
-    user.should_not be_valid
-  end
+    it 'requires a first_name' do
+      user.first_name = nil
+      user.should_not be_valid
+    end
 
-  it 'requires an email' do
-    user.email = nil
-    user.should_not be_valid
-  end
+    it 'requires an email' do
+      user.email = nil
+      user.should_not be_valid
+    end
 
-  it 'requires something that looks like an email' do
-    user.email = 'crap'
-    user.should_not be_valid
+    it 'requires something that looks like an email' do
+      user.email = 'crap'
+      user.should_not be_valid
+    end
+  
+    it 'emailable by default' do
+      user.should be_emailable
+    end
+  
+    it 'is not emailable' do
+      user.emailable = false
+      user.should_not be_emailable
+    end
+
+    it 'is not an admin by default' do
+      user.should_not be_an_admin
+    end
   end
   
-  it 'emailable' do
-    user.should be_emailable
-  end
-  
-  it 'is not emailable' do
-    user.emailable = false
-    user.should_not be_emailable
+  describe 'admin privileges' do
+    it 'is not an admin' do
+      users(:adam).should_not be_an_admin
+    end
+
+    it 'is an admin' do
+      users(:charlie).should be_an_admin
+    end
   end
   
 end
