4fce4e0389a8343bc8bda81fa5a927bdd49437c9
Michael Bamford <michael@michaelbamford.com>
2011-01-26 16:43:45 -0700

    Fixed background color or positivelyfrisky launch page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,7 @@
 class WelcomeController < ApplicationController
 
-  before_filter :require_no_user
-  before_filter :clear_lurker
+  before_filter :require_no_user, :only => [ :index ]
+  before_filter :clear_lurker, :only => [ :index ]
   
   def index
     store_reentry_location
