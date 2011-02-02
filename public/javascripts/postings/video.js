ec20d63a1bd572c013a98cc8e675c19153fe7076
Michael Bamford <michael@michaelbamford.com>
2011-02-02 03:42:11 -0700

    Added onPlayerError callback for youtube player

--- public/javascripts/postings/video.js
+++ public/javascripts/postings/video.js
@@ -12,7 +12,7 @@ jQuery(function($) {
 function onYouTubePlayerAPIReady() {
 	$('a', '.youtube_video_player').live('click', function(event) {
 		event.preventDefault();
-		$('img.youtube_thumb', this).hide();
+		$('img.youtube_thumb', this).fadeOut('slow');
 		new YT.Player($(this).closest('.youtube_video_player').get(0), {
 			height: '260',
 			width: '400',
@@ -20,7 +20,8 @@ function onYouTubePlayerAPIReady() {
 			playerVars: { disablekb: 1 },
 			events: {
 				'onReady': onPlayerReady,
-				'onStateChange': onPlayerStateChange
+				'onStateChange': onPlayerStateChange,
+				'onError': onPlayerError
 			}
 		});
 	});	
@@ -40,6 +41,10 @@ function onPlayerStateChange(event) {
   //   setTimeout(stopVideo, 6000);
   //   done = true;
   // }
+}
+
+function onPlayerError(event) {
+	alert(event.data);
 }
 
 function stopVideo() {
