162e8ee8b766426b5702cc5f766bba9b40fd777d
Michael Bamford <michael@michaelbamford.com>
2011-02-05 18:06:28 -0700

    Refactored comment css. Added fadeIn effects

--- public/javascripts/waves/comment.js
+++ public/javascripts/waves/comment.js
@@ -1,19 +1,29 @@
-jQuery(function($){
+jQuery(function($) {
 	
-	// Comment forms
-	 $('a.new_posting_comment').live('click', function(event) {
-	   event.preventDefault();
-	   $(this).hide().next('.comment-bubble').show();
-	 });
+	$('a.new_posting_comment').live('click', function(event) {
+		event.preventDefault();
+		$(this).fadeOut(function() {
+			$(this).next('.posting_comment')
+				.css({ opacity: 0.0 })
+				.slideDown(function() {
+					$(this).fadeTo('fast', 1.0);
+				});
+		});
+	});
 
-	 $('.comment-bubble form').bind('ajax:before', function(event) {
-	   $(this).hide().next('.new_posting_comment_spinner').show();
-	 });
+	$('.content form.new_posting_comment')
+		.bind('ajax:before', function(event) {
+			$(this).fadeOut(); // .next('.new_posting_comment_spinner').show();
+	});
 
 	 $('form.new_posting_comment')
-	    .find('button.cancel').live('click', function(event) {
-	      event.preventDefault();
-	      $(this).closest('.comment-bubble').hide().prev('a.new_posting_comment').show();
-	   });
-	
+		.find('button.cancel').live('click', function(event) {
+			event.preventDefault();
+			$(this).closest('.posting_comment').fadeTo('fast', 0.0, function() {
+				$(this)
+					.slideUp()
+					.prev('a.new_posting_comment')
+					.fadeIn();
+			});
+	   });	
 });
