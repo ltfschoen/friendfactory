056fb4368266bb9ad0558332a34987df03eb012d
Michael Bamford <michael@michaelbamford.com>
2011-02-09 04:59:01 -0700

    Animate new comment button. Float comments

--- public/javascripts/waves/comment.js
+++ public/javascripts/waves/comment.js
@@ -1,29 +1,50 @@
 jQuery(function($) {
 	
+	$('.posting_comment').bind('pulse', function(event) {
+		$(this).toggleClass('pulse');
+	});
+
+	
 	$('a.new_posting_comment').live('click', function(event) {
 		event.preventDefault();
-		$(this).fadeOut(function() {
-			$(this).next('.posting_comment')
+		$(this)
+			.fadeTo('fast', 0.0)
+			.next('.posting_comment')
 				.css({ opacity: 0.0 })
 				.slideDown(function() {
-					$(this).fadeTo('fast', 1.0);
-				});
-		});
+					$(this)
+						.fadeTo('fast', 1.0)
+						.find('textarea')
+							.focus();
+				});					
 	});
 
-	$('.content form.new_posting_comment')
+
+	$('form.new_posting_comment')
 		.bind('ajax:before', function(event) {
-			$(this).fadeOut(); // .next('.new_posting_comment_spinner').show();
-	});
-
-	 $('form.new_posting_comment')
-		.find('button.cancel').live('click', function(event) {
-			event.preventDefault();
-			$(this).closest('.posting_comment').fadeTo('fast', 0.0, function() {
+			$(this)
+				.closest('.posting_comment')
+				.trigger('pulse');
+		})
+		.bind('ajax:complete', function(event) {
+			$(this)
+				.closest('.posting_comment')
+				.trigger('pulse');
+		})
+		.find('button.cancel')
+			.live('click', function(event) {
+				event.preventDefault();
 				$(this)
-					.slideUp()
-					.prev('a.new_posting_comment')
-					.fadeIn();
-			});
-	   });	
+					.closest('.posting_comment')
+					.fadeTo('fast', 0.5, function() {
+						$(this)
+							.slideUp()
+							.prev('a.new_posting_comment')
+							.fadeTo('fast', 1.0);
+							// .css({ opacity: 1.0 });
+							// .show();
+							// .fadeIn('fast', 1.0);
+					});
+			});	
+
 });
