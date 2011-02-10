1d16bf248b5a79086b517ffbff632bf16b61801b
Michael Bamford <michael@michaelbamford.com>
2011-02-10 15:32:32 -0700

    Added dispalce for comments

--- app/helpers/posting/comments_helper.rb
+++ app/helpers/posting/comments_helper.rb
@@ -1,10 +1,14 @@
 module Posting::CommentsHelper
 
-  def render_comments(posting)    
-    content_tag(:div, :class => 'posting_comments') do
-      render(:partial => 'posting/comments/comment', :collection => posting.children.comments)
-    end + 
+  def render_comments(posting)
+    content_tag(:ul, :class => 'posting_comments') do
+      posting.children.comments.inject(String.new) do |buffer, comment|
+        buffer << content_tag(:li) do
+          render(:partial => 'posting/comments/comment', :object => comment)
+        end
+      end
+    end <<
     render(:partial => 'posting/comments/new', :locals => { :parent => posting })
-  end
+  end  
   
 end
