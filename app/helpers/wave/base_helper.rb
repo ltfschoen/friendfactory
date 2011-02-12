ce734f6efe5b7040e40ec75c52a4fc18ae7cbcca
Michael Bamford <michael@michaelbamford.com>
2011-02-12 03:20:12 -0700

    Post-its grouped per wave page

--- app/helpers/wave/base_helper.rb
+++ app/helpers/wave/base_helper.rb
@@ -1,6 +1,8 @@
 module Wave::BaseHelper
 
   def render_postings(postings, opts={})
+    return unless postings.present?
+    
     if opts[:only].present?
       postings = postings.select{ |posting| posting[:type] == opts[:only].to_s }
     end
@@ -9,11 +11,35 @@ module Wave::BaseHelper
       postings = postings.reject{ |posting| posting[:type] == opts[:exclude].to_s }
     end
 
-    content_tag(:ul, :class => opts[:class]) do
-      postings.inject(String.new) do |buffer, posting|
-        buffer << content_tag(:li) do
-          render(:partial => 'posting/posting', :object => posting)
-        end        
+    rendered_post_its = render_post_its(postings)
+    postings = postings.reject{ |posting| posting[:type] == Posting::PostIt.name }
+    number_of_breaks = (rendered_post_its.length > 0) ? rendered_post_its.length : postings.length    
+    
+    content_tag(:ul, :class => 'postings') do      
+      String.new.tap do |html|
+        postings.in_groups(number_of_breaks) do |postings_in_break|          
+          html << content_tag(:li) do
+            rendered_post_its.shift
+          end
+          html << postings_in_break.inject(String.new) do |buffer, posting|
+            buffer << content_tag(:li) do
+              render(:partial => 'posting/posting', :object => posting)
+            end
+          end
+        end
+      end
+    end
+  end
+  
+  def render_post_its(postings)
+    postings = postings.select{ |posting| posting[:type] == Posting::PostIt.name }
+    postings.in_groups_of(5).map do |group|
+      content_tag(:ul, :class => 'posting_post_its clearfix') do
+        group.compact.inject(String.new) do |buffer, posting|
+          buffer << content_tag(:li) do
+            render(:partial => 'posting/posting', :object => posting)
+          end
+        end
       end
     end
   end
