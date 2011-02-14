fba84d37fa4b7d3c8ddcc8d450514aa427bc34ab
Michael Bamford <michael@michaelbamford.com>
2011-02-14 14:02:57 -0700

    Render photos at their ingested size (removed 'small' css class)

--- app/helpers/posting/photos_helper.rb
+++ app/helpers/posting/photos_helper.rb
@@ -1,11 +1,14 @@
 module Posting::PhotosHelper
 
-  def render_photo(photo)
+  def render_photo(photo, opts = {})
     return '&nbsp;'.html_safe unless photo.present?
+    
+    css = [ 'photo', opts[:class] ].compact * ' '
+    
     if photo.horizontal?
-      image_tag(photo.image.url(:h4x6), :site => false, :class => 'photo h4x6 small')
+      image_tag(photo.image.url(:h4x6), :site => false, :class => "#{css} h4x6")
     else
-      image_tag(photo.image.url(:v4x6), :site => false, :class => 'photo v4x6 small')
+      image_tag(photo.image.url(:v4x6), :site => false, :class => "#{css} v4x6")
     end
   end
 
