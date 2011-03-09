f656fcd87288bf163b0b6b398d1a8755a99012c2
Michael Bamford <michael@michaelbamford.com>
2011-03-09 11:05:29 +1100

    Call buttonize on comment form

--- public/javascripts/waves/comment.js
+++ public/javascripts/waves/comment.js
@@ -4,40 +4,39 @@ jQuery(function($) {
 		singleMode: true, 
 		itemSelector: '.posting_comment:visible'
 	});
-	
-	$('button', 'form.new_posting_comment').button({ text: false })
-	
-	
+
 	$('a.new_posting_comment').live('click', function(event) {
 		event.preventDefault();
-		$(this)
-			.fadeTo('fast', 0.0, function() {
-				$(this)
-					.closest('.posting')
-					.find('.posting_comment.form')
-						.css({ opacity: 0.0 })
-						.slideDown(function() {
-							$(this)
-								.fadeTo('fast', 1.0)
-								.find('textarea')
-									.val('').focus();
-						});				
-			})
+		$(this).fadeTo('fast', 0.0, function() {
+			$(this).closest('.posting')
+				.find('.posting_comment.form')
+					.css({ opacity: 0.0 })
+					.slideDown(function() {
+						$(this).fadeTo('fast', 1.0)
+							.find('textarea')
+								.val('').focus();
+					});				
+		});
 	});
 
 
 	$('form.new_posting_comment')
+		.buttonize()
+		
 		.bind('ajax:before', function(event) {
 			$(this).find('.button-bar')
 				.css({ opacity: 0.0 });
 		})
+		
 		.bind('ajax:complete', function(event) {
 			$(this).find('.button-bar')
 				.css({ opacity: 1.0 });
 		})
+		
 		.find('textarea')
 			.autoResize({ extraSpace: 12, limit: 152 })
 		.end()
+		
 		.find('button.cancel')
 			.live('click', function(event) {
 				event.preventDefault();
