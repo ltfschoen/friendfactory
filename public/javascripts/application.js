4927e13f7749d4188c026353829e8e1dce146d24
Michael Bamford <michael@michaelbamford.com>
2011-03-09 18:16:47 +1100

    Override fadeTo() and hide() for IE

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -16,10 +16,49 @@ if (/chrome/.test(navigator.userAgent.toLowerCase())) {
 			.find('button.cancel')
 				.button({ icons: { primary: 'ui-icon-close' }, text: options.text })
 			.end();
-	};
-	
+	};	
+
 	$.fn.buttonize.defaults = {
 		text: false
+	};
+
+
+	var aliasedFadeTo = $.fn.fadeTo;
+	
+	$.fn.fadeTo = function() {
+		if ($.browser.msie) {
+			var $this = $(this).css({ opacity: 'auto' }),
+				opacity = arguments[1],
+				callBack;
+			
+			if ($.isFunction(arguments[arguments.length - 1])) {
+				callBack = arguments[arguments.length - 1];
+			}						
+			
+			return (opacity === 0) ? $this.hide(0, callBack) : $this.show(0, callBack);
+			
+		} else {
+			return aliasedFadeTo.apply(this, arguments);			
+		}
+	};
+	
+	
+	var aliasedHide = $.fn.hide;
+	
+	$.fn.hide = function() {
+		if ($.browser.msie) {
+			var $this = $(this).css({ opacity: 'auto' }),
+				callBack;
+
+			if ((arguments.length > 0) && $.isFunction(arguments[arguments.length - 1])) {
+				callBack = arguments[arguments.length - 1];
+			}
+			
+			return aliasedHide.apply(this, [ 0, callBack ]);
+
+		} else {
+			return aliasedHide.apply(this, arguments);			
+		}
 	};
 	
 })(jQuery);
