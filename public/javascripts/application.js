52f9bf9d90799c7507f90cba92995f6e974da6cd
Michael Bamford <michael@michaelbamford.com>
2011-03-09 11:04:43 +1100

    Removed button call on all forms. Created buttonize function

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -3,12 +3,21 @@ if (/chrome/.test(navigator.userAgent.toLowerCase())) {
   jQuery.browser.safari = false;
 }
 
-jQuery(document).ready(function($) {
- 	$('button.cancel, a.cancel')
-		.button({ icons: { primary: 'ui-icon-close' }});
-	
-	$('input, textarea').placehold();
-});
+// jQuery(function($) {
+//  	$('button.cancel, a.cancel')
+// 		.button({ icons: { primary: 'ui-icon-close' }});	
+// 	$('input, textarea').placehold();	
+// });
 
+(function($) {
+	$.fn.buttonize = function() {
+		return $(this)
+			.find('button[type="submit"]')
+				.button({ icons: { primary: 'ui-icon-check' }, text: false })
+			.end()
 
-
+			.find('button.cancel')
+				.button({ icons: { primary: 'ui-icon-close' }, text: false })
+			.end();
+	};
+})(jQuery);
