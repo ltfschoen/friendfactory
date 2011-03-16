3c5ea903db430466405680d9a835be6f3afa4517
Michael Bamford <michael@michaelbamford.com>
2011-03-16 14:11:06 +1100

    Added community default 'popular' waves for PF and FS

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,27 +6,20 @@
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
 
-Wave::Community.find_or_create_by_slug(
+Wave::Community.find_or_create_by_site_id_and_slug(
+    :site_id     => FriskyhandsSite.new.id,
     :slug        => Wave::CommunitiesController::DefaultWaveSlug,
-    :topic       => "Everyone's Wall",
-    :description => 'Get going!')
+    :topic       => 'FriskyHands Wave',
+    :description => '')
 
+Wave::Community.find_or_create_by_site_id_and_slug(
+    :site_id     => PositivelyfriskySite.new.id,
+    :slug        => Wave::CommunitiesController::DefaultWaveSlug,
+    :topic       => 'PositivelyFrisky Wave',
+    :description => '')
 
-[ [ 'nyc', 'new york' ],
-  [ 'la', 'los angeles' ],
-  [ 'sf', 'san francisco' ],
-  ['usa'], ['us'],
-  ['uk'],
-  ['australia'],
-  ['canada'],
-  ['al'], ['ak'], ['as'], ['az'], ['ar'], ['ca'], ['co'], ['ct'], ['de'],
-  ['dc'], ['fm'], ['fl'], ['ga'], ['gu'], ['hi'], ['id'], ['il'], ['in'], ['ia'],
-  ['ks'], ['ky'], ['la'], ['me'], ['mh'], ['md'], ['ma'], ['mi'], ['mn'], ['ms'],
-  ['mo'], ['mt'], ['ne'], ['nv'], ['nh'], ['nj'], ['nm'], ['ny'], ['nc'], ['nd'],
-  ['mp'], ['oh'], ['ok'], ['or'], ['pw'], ['pa'], ['pr'], ['ri'], ['sc'], ['sd'],
-  ['tn'], ['tx'], ['ut'], ['vt'], ['vi'], ['va'], ['wa'], ['wv'], ['wi'], ['wy'] ].each do |transpose|
-  Admin::Tag.find_or_create_by_defective(:defective => transpose[0], :corrected => transpose[1]) do |tag|
-    tag.taggable_type = 'UserInfo'
-  end
-end
-
+Wave::Community.find_or_create_by_site_id_and_slug(
+    :site_id     => FriskysoldiersSite.new.id,
+    :slug        => Wave::CommunitiesController::DefaultWaveSlug,
+    :topic       => 'FriskySoldiers Wave',
+    :description => '')
