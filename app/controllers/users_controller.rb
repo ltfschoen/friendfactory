fdca64105b3e1c12449901497f0e470a15534f9b
Michael Bamford <michael@michaelbamford.com>
2011-03-17 16:00:35 +1100

    Include site for created user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(params[:user])
+    @user.sites << current_site
     respond_to do |format|      
       if @user.save
         format.html { redirect_to root_path }
