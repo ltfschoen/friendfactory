36e63335e7eb0ad1f694f21239d65b18a9b4f2be
Michael Bamford <michael@michaelbamford.com>
2011-03-25 18:19:39 +1100

    New user model to support multiple profiles

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,19 +2,19 @@ class User < ActiveRecord::Base
 
   include ActiveRecord::Transitions
 
-  attr_reader :current_site, :invitation_code
+  attr_reader :invitation_code, :recently_added_site, :recently_added_profile
 
-  validates_presence_of :handle
-  validates_presence_of :current_site, :on => :create
-
+  validates_presence_of :recently_added_profile, :on => :create
+  
   validates_each :invitation_code,
-      :on => :create,
-      :if => lambda { |user| current_site && current_site.invite_only? },
-      :message => 'is not a valid membership code' do |user, attr, value|
-    unless user.invitations.where(:code => value, :site_id => user.current_site.id).present?
-      user.errors.add(attr)
+      :if => lambda { |user| user.recently_added_site && user.recently_added_site.invite_only? },
+      :message => 'is not a valid invitation code' do |user, attribute, value|
+    unless user.invitations.where(:code => value, :site_id => user.recently_added_site.id).present?
+      user.errors.add(attribute)
     end
   end
+    
+  validates_associated :profiles
   
   acts_as_authentic do |config|
     config.logged_in_timeout UserSession::InactivityTimeout
@@ -32,34 +32,19 @@ class User < ActiveRecord::Base
     end
   end
   
-  has_and_belongs_to_many :sites, :before_add => :validates_uniqueness_of_site
-
-  def validates_uniqueness_of_site(site)    
-    raise "User already on site" if self.sites.where(:id => site.id).present?
-  end
-
-  def current_site=(site)
-    @current_site ||= begin
-      self.sites << site rescue nil
-      site
-    end
-  end
-        
-  has_many :waves, :class_name => 'Wave::Base'
-  has_many :profiles, :class_name => 'Wave::Profile'  
+  scope :online, lambda { { :conditions =>
+      [ 'last_request_at >= ? and current_login_at is not null', (Time.now - UserSession::InactivityTimeout).to_s(:db) ] } }
   
-  def profile(site)
-    profiles.joins(:sites).where(:sites => { :id => site.id }).order('updated_at desc').limit(1).first
-  end
-      
-  def user_info(site)
-    profile(site).profile_info
-  end
-    
-  has_many :conversations,
-      :class_name => 'Wave::Conversation',
-      :order      => 'created_at desc' do        
-    def site(site)      
+  has_and_belongs_to_many :sites, :uniq => true,
+      :before_add => :validate_not_already_on_site,
+      :after_add  => :set_recently_added_site
+
+  has_many :waves, :class_name => 'Wave::Base'  
+  has_many :profiles, :class_name => 'Wave::Profile'    
+  has_many :inboxes, :class_name => 'Wave::Inbox'
+  
+  has_many :conversations, :class_name => 'Wave::Conversation', :order => 'created_at desc' do        
+    def site(site)
       joins(:sites).where('sites_waves.site_id = ?', site.id) if site.present?
     end
     def with(receiver, site)
@@ -71,6 +56,82 @@ class User < ActiveRecord::Base
   # <user1>.conversation.with.<user2>
   alias :conversation :conversations
 
+  has_many :postings, :class_name => 'Posting::Base'
+  has_many :invitations, :foreign_key => 'email', :primary_key => 'email'    
+  has_many :sponsorships, :class_name  => 'Invitation', :foreign_key => 'sponsor_id'
+
+  # has_many :friendships
+  # has_many :friends,  :through => :friendships
+  # has_many :inverse_friendships, :class_name => 'Friendship', :foreign_key => 'friend_id'
+  # has_many :admirers, :through => :inverse_friendships, :source => :user
+
+  def validate_not_already_on_site(site)
+    raise "User already on site" if sites.where(:id => site.id).present?
+  end
+
+  def set_recently_added_site(site)
+    raise "No profile available to add to site" unless defined?(@recently_added_profile)
+    @recently_added_profile.sites << site
+    @recently_added_site = site
+  end
+
+
+  # ===  Build assocations ===
+  
+  def invitation=(attrs)
+    @invitation_code = attrs["code"]
+  end
+    
+  def profile=(attrs)
+    build_profile(attrs)
+  end
+
+  def build_profile(attrs)
+    @recently_added_profile = profiles.build(attrs)
+  end
+  
+
+  # === Profile ===
+
+  def profile(site)
+    profiles.joins(:sites).where(:sites => { :id => site.id }).order('updated_at desc').limit(1).first
+  end
+  
+  def avatar(site)
+    profile(site).avatar
+  end
+  
+  def handle(site)
+    profile(site).handle
+  end
+  
+  def first_name(site)
+    profile(site).first_name
+  end
+  
+  def last_name(site)
+    profile(site).last_name
+  end
+  
+  def full_name(site)
+    profile(site).full_name
+  end
+  
+  def gender(site)
+    profile(site).gender
+  end
+  
+  
+  # TODO Delete this. Should not expose the resource relationship.. who cares
+  # about a profile's resource?? The profile should expose the interesting
+  # things diretly.
+  def user_info(site)
+    profile(site).resource
+  end
+
+
+  # === Conversations ===
+  
   def create_conversation_with(receiver, site)
     if receiver.present? && site.present?
       wave = conversations.build
@@ -80,8 +141,6 @@ class User < ActiveRecord::Base
       wave
     end
   end
-
-  has_many :inboxes, :class_name => 'Wave::Inbox'
 
   def inbox(site)
     if site.present?
@@ -99,38 +158,12 @@ class User < ActiveRecord::Base
       inbox
     end
   end
+
+
+  # ===
   
-  has_many :postings, :class_name => 'Posting::Base'
-    
-  has_many :invitations, :foreign_key => 'email', :primary_key => 'email'
-
-  def invitations_attributes=(attrs)
-    @invitation_code ||= attrs["0"]["code"] rescue nil
-  end
-    
-  has_many :sponsorships, :class_name  => 'Invitation', :foreign_key => 'sponsor_id'
-    
-  has_many :friendships
-  has_many :friends,  :through => :friendships
-  has_many :inverse_friendships, :class_name => 'Friendship', :foreign_key => 'friend_id'
-  has_many :admirers, :through => :inverse_friendships, :source => :user
-
-  scope :online, :conditions => [ 'last_request_at >= ? and current_login_at is not null', (Time.now - UserSession::InactivityTimeout).to_s(:db) ]
-
-  after_create do |user|
-    create_profile(user.current_site)
-  end
-  
-  def initialize_profile(site)
-    create_profile(site) if profile(site).nil?
-  end
-
-  def create_profile(site)
-    profiles.new.tap { |profile| site.waves << profile }
-  end
-
-  def avatar(site)
-    profile(site).avatar
+  def self.find_or_create_with_profile(params)
+    existing_user_with_profile(params) || create_user_with_profile(params)
   end
   
   def self.online?(user)
@@ -141,65 +174,25 @@ class User < ActiveRecord::Base
     User.online?(self)
   end
   
-  def has_friend?(buddy)
-    self.friendships.map(&:friend_id).include?(buddy.id)
-  end
-  
-  # DefaultMessagePeriod = 10.days
-  
-  # has_many :received_messages, :class_name => Message.name, :foreign_key => 'receiver_id', :order => 'created_at desc' do
-  #   def since(period)
-  #     find :all, :conditions => [ 'created_at >= ?', (Time.now - period).to_s(:db) ], :include => [ :sender ]
-  #   end
-  # end
-  
-  # has_many :sent_messages, :class_name => Message.name, :foreign_key => 'sender_id', :order => 'created_at desc' do
-  #   def since(period)
-  #     find :all, :conditions => [ 'created_at >= ?', (Time.now - period).to_s(:db) ]
-  #   end
-  # end
-  
-  # has_many :senders,    :through => :received_messages, :source => :sender,   :uniq => true
-  # has_many :recipients, :through => :sent_messages,     :source => :receiver, :uniq => true
-
-  # def message_threads(period = DefaultMessagePeriod)    
-  #   # Returns an array of user's message threads. Each thread is an
-  #   # array of two elements: the leaf message, and an array of the
-  #   # leaf node ancestors.
-  #   
-  #   # First, get the user's received leaf messages
-  #   messages = self.received_messages.since(period).inject([]) do |messages, message|
-  #     message.children.empty? ? messages << [ message, message.ancestors ] : messages
-  #   end
-  #   
-  #   # Then, get the user's sent leaf messages
-  #   self.sent_messages.since(period).inject(messages) do |messages, message|
-  #     message.children.empty? ? messages << [ message, message.ancestors ] : messages
-  #   end        
-  # end
-
-  # def send_message(attrs = {})
-  #   self.sent_messages.create(attrs)
-  # end
-
-  def handle(site)
-    read_attribute(:handle) || first_name
-  end
-
-  def first_name(site)
-    read_attribute(:first_name).try(:humanize)
-  end
-  
-  def full_name(site)
-    [ read_attribute(:first_name).try(:humanize), read_attribute(:last_name).try(:humanize) ].compact * ' '
-  end
-    
   def to_s
     "{ :id => #{self.id}, :full_name => #{full_name} }"
   end
   
   def reset_password!
     reset_perishable_token!
+  end
+  
+  private
+  
+  def self.existing_user_with_profile(params)
+    if user = User.find_by_email(params[:email])
+      user.build_profile(params[:profile])
+      user
+    end
+  end
+  
+  def self.create_user_with_profile(params)
+    User.new(params)    
   end
   
 end
