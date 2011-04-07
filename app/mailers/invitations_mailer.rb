bbf7fc7943221b930de349d4c9365047a9755fa8
Michael Bamford <michael@michaelbamford.com>
2011-04-07 17:08:53 +1000

    Invitation mail views

--- app/mailers/invitations_mailer.rb
+++ app/mailers/invitations_mailer.rb
@@ -1,6 +1,6 @@
 class InvitationsMailer < ActionMailer::Base
   
-  # default :from => "michael@friskyhands.com"
+  default :from => "michael@friskyhands.com"
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
@@ -9,14 +9,8 @@ class InvitationsMailer < ActionMailer::Base
   #
 
   def new_invitation_mail(invitation)
-    @invitation = invitation    
-
-    email = case Rails.env
-    when 'production' then @invitation.email
-    when 'staging'    then @invitation.sponsor.email
-    else 'michael@michaelbamford.com'
-    end
-
+    @invitation = invitation
+    email = Rails.env.production? ? @invitation.email : 'michael@michaelbamford.com'
     mail :to => email, :subject => "Invitation to join #{@invitation.site}"
   end
   
--- app/mailers/messages_mailer.rb
+++ app/mailers/messages_mailer.rb
@@ -1,12 +1,10 @@
 class MessagesMailer < ActionMailer::Base
 
-  helper :postings
-
   default :from => "michael@friskyhands.com"
 
   def new_message_notification(message)
     @message = message
-    email = Rails.env.development? ? 'michael@michaelbamford.com' : @message.receiver.email    
+    email = Rails.env.production? ? @message.receiver.email : 'michael@michaelbamford.com'
     #TODO Add 'at FriskyHands' to subject
     mail :to => email, :subject => "Message from #{@message.sender.handle(@message.site)}"
   end
