8c1415112472a78020b943c31865e336bac63bfe
Michael Bamford <michael@michaelbamford.com>
2011-04-14 17:55:17 +1000

    Messages urls include host name

--- app/mailers/messages_mailer.rb
+++ app/mailers/messages_mailer.rb
@@ -1,12 +1,11 @@
 class MessagesMailer < ActionMailer::Base
 
-  default :from => "michael@friskyhands.com"
-
   def new_message_notification(message)
     @message = message
     email = Rails.env.production? ? @message.receiver.email : 'michael@michaelbamford.com'
-    #TODO Add 'at FriskyHands' to subject
-    mail :to => email, :subject => "Message from #{@message.sender.handle(@message.site)}"
+    mail :to   => email,
+      :from    => "messages@#{message.site.name}.com",
+      :subject => "Message from #{@message.sender.handle(@message.site)} at #{@message.site.display_name}"
   end
 
 end
