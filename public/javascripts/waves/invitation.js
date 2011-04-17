6d0c08592c9b188e0484509a3daba85757e5fa7e
Michael Bamford <michael@michaelbamford.com>
2011-04-17 22:16:43 +1000

    Fixed bug to how text input for invitation edit

--- public/javascripts/waves/invitation.js
+++ public/javascripts/waves/invitation.js
@@ -1,26 +1,4 @@
 jQuery(function($) {
-
-	$('.polaroid.edit img.thumb', '.cssanimations .wave_invitation')
-		.bind('pulse', function() {
-			$(this).toggleClass('pulse');
-		});
-		
-	$('.polaroid.edit img.thumb', '.no-cssanimations .wave_invitation')
-		.bind('pulse', function() {
-			var $this = $(this),
-				worker;
-			
-			if (!$this.hasClass('pulse')) {
-				worker = setInterval(function() {
-					$this.fadeTo(300, 0.5).fadeTo(300, 1.0);
-				}, 600);
-				$this.attr('data-worker', worker);
-
-			} else {
-				clearInterval($this.attr('data-worker'));
-				$this.removeAttr('data-worker');
-			}
-		});
 		
 	$('.wave_invitation')
 		.find('.polaroid.edit a.new_posting_invitation, .polaroid.edit a.edit_posting_invitation')
@@ -32,6 +10,7 @@ jQuery(function($) {
 					method = $target.data('method');
 
 				event.preventDefault();
+
 				$target
 					.closest('.polaroid')
 						.find('form').attr('action', url)
@@ -48,9 +27,10 @@ jQuery(function($) {
 					eq = $this.find('input#li_eq').val();
 					
 				$this
-					.find('.button-bar').hide().end()
-					.find('li:eq(' + eq + ') img.thumb')
-						.trigger('pulse');
+					.find('.button-bar')
+						.hide()
+					.end()					
+					.find('li:eq(' + eq + ') img.thumb').pulse();
 			})
 			
 			.find('button.cancel')
