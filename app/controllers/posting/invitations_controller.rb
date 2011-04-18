2a83d4a41714b488522b44f94c4194624bcf9c51
Michael Bamford <michael@michaelbamford.com>
2011-04-18 10:17:12 +1000

    Fixed bug in update posting invitations

--- app/controllers/posting/invitations_controller.rb
+++ app/controllers/posting/invitations_controller.rb
@@ -15,7 +15,7 @@ class Posting::InvitationsController < ApplicationController
     @li_eq = params[:li_eq]
     if @wave = current_user.find_invitation_wave_by_id(params[:wave_id])
       @posting = @wave.postings.find_by_id(params[:id])
-      @posting.try(:update_attributes(params[:posting_invitation]))
+      @posting.try(:update_attributes, params[:posting_invitation])
     end
     respond_to do |format|
       format.js { render :action => 'create', :layout => false }
