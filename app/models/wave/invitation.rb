2cc24e7682f9808e50c9f2d1fefc203c12aa1547
Michael Bamford <michael@michaelbamford.com>
2011-05-05 11:00:51 +1000

    Created Wave::Invitation.find_all_by_site_and_fully_offered

--- app/models/wave/invitation.rb
+++ app/models/wave/invitation.rb
@@ -1,2 +1,11 @@
 class Wave::Invitation < Wave::Base
+
+  def self.find_all_by_site_and_fully_offered(site, min_offered = Wave::InvitationsHelper::MaximumDefaultImages)
+    select('`waves`.*').
+    joins('INNER JOIN `postings_waves` ON `waves`.`id` = `postings_waves`.`wave_id`').
+    group('`postings_waves`.`wave_id`').
+    having("count(*) >= #{min_offered.to_i}").
+    site(site)
+  end
+  
 end
--- spec/models/wave/invitation_spec.rb
+++ spec/models/wave/invitation_spec.rb
@@ -1,5 +1,25 @@
 require 'spec_helper'
 
 describe Wave::Invitation do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  fixtures :sites, :waves
+  set_fixture_class :waves => 'Wave::Base'
+
+  it "finds fully offered invitation waves" do
+    Wave::Invitation.find_all_by_site_and_fully_offered(sites(:positivelyfrisky), 2).should == [ waves(:adam_invitation_wave) ]
+    Wave::Invitation.find_all_by_site_and_fully_offered(sites(:positivelyfrisky), 3).should == [ waves(:adam_invitation_wave) ]
+    
+  end
+  
+  it "doesn't find invitations not fully offered" do
+    Wave::Invitation.find_all_by_site_and_fully_offered(sites(:positivelyfrisky), 4).should == []
+  end
+
+  it "find invitations fully offered for one site only" do
+    Wave::Invitation.find_all_by_site_and_fully_offered(sites(:positivelyfrisky), 2).
+        map(&:sites).flatten.
+        map(&:name).uniq.
+        should == [ 'positivelyfrisky' ]
+  end
+  
 end
