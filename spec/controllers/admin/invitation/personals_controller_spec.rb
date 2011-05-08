a38e44843ddd589ce9cc8227c08b1d66734a8b25
Michael Bamford <michael@michaelbamford.com>
2011-05-08 15:20:36 +1000

    Refactored personals controller admin login tests

--- spec/controllers/admin/invitation/personals_controller_spec.rb
+++ spec/controllers/admin/invitation/personals_controller_spec.rb
@@ -4,31 +4,28 @@ describe Admin::Invitation::PersonalsController do
   
   fixtures :sites, :users
   
-  let(:wave) { mock_model(Wave::Invitation).as_null_object }
-
-  describe 'administrator' do
-    before(:each) do
-      login(:friskyhands, :adam)
-    end
-
-    describe "GET index" do
-      it "assigns invitation waves to @waves" do
-        Wave::Invitation.should_receive(:find_all_by_site_and_fully_offered).and_return([ wave ])
-        get :index
-        assigns(:waves).should eq([ wave ])
-      end
-    end
-  end
-  
-  describe 'not an administrator' do
-    before(:each) do
-      login(:friskyhands, :bert)
+  describe "requires an administrator" do
+    it "to access" do
+      login(:positivelyfrisky, :adam)
+      response.should be_successful
     end
     
-    it "redirects to welcome" do
+    it "otherwise disallows" do
       get :index
       response.should redirect_to(welcome_url)
     end
   end
 
+  describe 'GET index' do    
+    let(:wave) { mock_model(Wave::Invitation).as_null_object }
+        
+    before(:each) { login(:friskyhands, :adam) }
+
+    it "assigns invitation waves to @waves" do
+      Wave::Invitation.should_receive(:find_all_by_site_and_fully_offered).and_return([ wave ])
+      get :index
+      assigns(:waves).should eq([ wave ])
+    end
+  end
+  
 end
