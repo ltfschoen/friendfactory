0d0c06cf70c1799e7ba4821e94361858a7655655
Michael Bamford <michael@michaelbamford.com>
2011-05-10 16:28:33 +1000

    Added universal invitations index and spec view

---
+++ spec/views/admin/invitation/universals/index.haml_spec.rb
@@ -0,0 +1,30 @@
+require 'spec_helper'
+
+describe 'admin/invitation/universals/index.haml' do
+  
+  set_fixture_class :postings => 'Posting::Base'
+  fixtures :sites, :postings
+
+  let(:invitation) { postings(:invitation_posting_for_anonymous) }
+
+  before(:each) do
+    view.stub!(:current_site).and_return(sites(:positivelyfrisky))
+    assign(:postings, [ invitation ])
+  end  
+  
+  it "renders inviter's handle" do
+    render
+    rendered.should contain('AdamA')
+  end
+
+  it "renders universal invitation code" do
+    render
+    rendered.should contain('e5')
+  end
+  
+  it "renders the invitation's state" do
+    render
+    rendered.should contain('Offered')    
+  end
+  
+end
