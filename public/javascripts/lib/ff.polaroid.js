4f210e461f7281371017c805391237c2cc3c58a5
Michael Bamford <michael@michaelbamford.com>
2011-06-03 15:20:39 +1000

    Headshot seeks to pane for border flip when first initialized on a pane

--- public/javascripts/lib/ff.polaroid.js
+++ public/javascripts/lib/ff.polaroid.js
@@ -91,14 +91,20 @@
 				}				
 			}); // each
 			
-		} else {
-						
-			return this.each(function() {				
-				var $this = $(this); // a polaroid
-				
-				$this.find('.face-container:eq(1)').hide();
-				// $this.find('.face-container:eq(0)').hide();
-				
+		} else {			
+			// no-csstransforms3d
+			return this.each(function() {
+				var $this = $(this), // a polaroid				
+					scrollableSettings = {
+						items: 'items',
+						keyboard: false,
+						next: '',
+						prev: '',							
+	   	    			onSeek: function(event, idx) {
+							panes.init(this, idx);
+	   	    			}						
+					};				
+								
 				if (settings['close-button'] === true) {
 					$('a.close', $this).live('click', function(event) {
 						$this.fadeOut();
@@ -107,46 +113,95 @@
 					});
 				}				
 
-				$this.find('.front .buddy-bar a.flip').live('click', function(event) {
-		    		event.preventDefault();
-					
-					var $polaroid = $(this).closest('.polaroid');
-					$polaroid.data('scrollable-index', $(this).closest('li').index());
-					
-		    		$polaroid.flip({
-						speed: 300,
-		      			direction: 'lr',
-		      			color: '#FFF',
-		      			content: $polaroid.find('.face-container:hidden'),
-				
-		      			onEnd: function() {
-					  		$polaroid.find('.buddy-bar a.flip').click(function(event) {
-								event.preventDefault();
-								$polaroid.revertFlip();								
-							});
+				if (settings['pane'] === undefined) {
+					$this
+						.find('.face-container:eq(1)')
+							.hide()
+						.end()
+						.find('.face-container:eq(0) .buddy-bar a.flip')
+							.live('click', function(event) {
+								var $polaroid = $(this).closest('.polaroid');
+					    		event.preventDefault();
+								$polaroid
+									.data('scrollable-index', $(this).closest('li').index())
+									.flip({
+										speed: 300,
+						      			direction: 'lr',
+						      			color: '#FFF',
+						      			content: $polaroid.find('.face-container:hidden'),				
+						      			onEnd: function() {
+									  		$polaroid
+												.find('.buddy-bar a.flip')
+													.click(function(event) {
+														event.preventDefault();
+														$polaroid.revertFlip();								
+													});
 
-							// Make links work on backside
-							$polaroid.find('.scrollable').scrollable({
-								items: 'items',
-								keyboard: false,
-								next: '',
-								prev: '',							
-	      	    				onSeek: function(event, idx) {
-									panes.init(this, idx);
-	      	    				}
-							}).navigator();
+											// Make links work on backside												
+											$polaroid.find('.scrollable')
+												.scrollable(scrollableSettings)
+												.navigator();
 
-							var idx = $polaroid.data('scrollable-index');
-							$polaroid.find('.scrollable').scrollable().seekTo(idx ,0);
-		      			} // onEnd		
-		    		}); // flip
-		
-		  		}); // live
-		
-			}); // each
-			
-		} // if
-		
+											var idx = $polaroid.data('scrollable-index');
+											$polaroid.find('.scrollable').scrollable().seekTo(idx, 0);
+						      			} // onEnd		
+						    		}); // flip		
+					  		}); // live
+
+				} else {
+					var flippery = function(event) {
+							var $polaroid = $(event.target).closest('.polaroid');	
+							event.preventDefault();
+							$polaroid
+								.flip({
+									speed: 300,
+					      			direction: 'lr',
+					      			color: '#FFF',
+					      			content: $polaroid.find('.face-container:hidden'),
+									onEnd: function() {
+										$polaroid
+											.find('.front.face:not(:hidden) .buddy-bar a.flip')
+												.bind('click', function(event) {
+													event.preventDefault();
+													$polaroid
+														.data('scrollable-index', $(event.target).closest('li').index())																										
+														.revertFlip();
+												})
+											.end()
+								
+											.find('.back.face:not(:hidden)')
+											 	.find('.buddy-bar a.flip')
+													.bind('click', flippery)
+												.end()
+												.find('.scrollable')
+													.scrollable(scrollableSettings)
+													.navigator();
+										
+										if ($polaroid.find('.back.face').length == 1) {
+											var idx = $polaroid.data('scrollable-index');
+											$polaroid.find('.scrollable').scrollable().seekTo(idx ,0);											
+										}
+									} // onEnd
+								});
+						};
+					
+					$this
+						.find('.face-container:eq(0)')
+							.hide()
+						.end()						
+						.find('.face-container:eq(1)')
+							.find('.buddy-bar a.flip')
+								.click(flippery)
+							.end()						
+							.find('.scrollable')
+								.scrollable(scrollableSettings)
+								.navigator();
+						
+					$this.find('.scrollable').scrollable().seekTo(settings['pane'], 0);
+					
+				} // if		
+			}); // each			
+		} // if		
 	}; // fn.polaroid
 	
 })(jQuery);
