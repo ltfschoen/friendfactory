f60cb41384e5882982d6796f4848252fd6dcf087
Michael Bamford <michael@michaelbamford.com>
2011-06-06 13:24:13 +1000

    Refactored javascript app directory is minic controller namespace

--- public/javascripts/app/admin/sites.js
+++ public/javascripts/app/admin/sites.js
@@ -5,24 +5,6 @@ jQuery(function($) {
 '<div class="tr asset"><div class="td silhouette"><img alt="Silhouette-q" height="32" src="/images/friskyfactory/silhouette-q.gif" width="32"></div><div class="td"><input id="site_assets_attributes_'+ seq + '_name" name="site[assets_attributes][' + seq + '][name]" placeholder="variable" size="30" type="text"><input id="site_assets_attributes_' + seq + '_asset" name="site[assets_attributes][' + seq + '][asset]" type="file"></div><div class="td">&nbsp;</div></div>');
 	}
 	
-	$('.admin.invitation')
-		.buttonize()
-		
-		.find('form')
-			.bind('ajax:before', function() {
-				$(this).find('.spinner').css({ 'visibility': 'visible' });
-			})
-		
-			.bind('ajax:success', function(event, status) {
-				var success = status['updated'];
-				$(this).find('.spinner').css({ 'visibility': 'hidden' });
-			})
-		
-			.find("select[name='posting_invitation[state]']")
-				.change(function() {
-					$(this).closest('form').trigger('submit');
-				});
-	
 	$('.admin.site')
 		.buttonize({ text: true })
 		
