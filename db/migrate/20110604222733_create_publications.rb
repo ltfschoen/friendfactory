6ad51cc11f7c70866cd02d597a6d3441ef7039ea
Michael Bamford <michael@michaelbamford.com>
2011-06-08 08:31:59 +1000

    Feedback messages on postings_waves migration

--- db/migrate/20110604222733_create_publications.rb
+++ db/migrate/20110604222733_create_publications.rb
@@ -7,13 +7,16 @@ class CreatePublications < ActiveRecord::Migration
       t.timestamps
     end
     add_index :publications, [ :wave_id, :resource_id ]
-    add_index :publications, [ :resource_id ]    
+    add_index :publications, [ :resource_id ]
+    say 'migrating postings_waves to publications'
     ActiveRecord::Base.connection.select_all('select posting_id, wave_id from postings_waves').each do |posting_wave|
       ::Publication.create!(:wave_id => posting_wave['wave_id'], :resource_id => posting_wave['posting_id'], :resource_type => 'Posting::Base')
-    end    
+    end
+    rename_table :postings_waves, :postings_waves_as_habtm
   end
 
   def self.down
+    rename_name :postings_waves_as_habtm, :postings_waves
     drop_table :publications
   end
 end
