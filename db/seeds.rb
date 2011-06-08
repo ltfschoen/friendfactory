fd73ca40bdeaf5631655bde58ee346e95e86a262
Michael Bamford <michael@michaelbamford.com>
2011-06-08 14:00:55 +1000

    Removed create default wave from seed (down as after_create on Site)

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,25 +2,25 @@ Site.find_or_create_by_name(
     :name                     => 'friskyhands',
     :display_name             => 'FriskyHands',
     :analytics_account_number => 'UA-19948002-1',
-    :analytics_domain_name    => '.friskyhands.com').create_default_wave
+    :analytics_domain_name    => '.friskyhands.com')
 
 Site.find_or_create_by_name(
     :name                     => 'positivelyfrisky',
     :display_name             => 'PositivelyFrisky',
     :invite_only              => true,
     :analytics_account_number => 'UA-19948002-2',
-    :analytics_domain_name    => '.positivelyfrisky.com').create_default_wave
+    :analytics_domain_name    => '.positivelyfrisky.com')
 
 Site.find_or_create_by_name(
     :name                     => 'friskyforces',
     :display_name             => 'FriskyForces',
     :launch                   => true,
     :analytics_account_number => 'UA-19948002-3',
-    :analytics_domain_name    => '.friskyforces.com').create_default_wave
+    :analytics_domain_name    => '.friskyforces.com')
 
 Site.find_or_create_by_name(
     :name                     => 'dizmdayz',
     :display_name             => 'Dizmdayz',
     :launch                   => false,
     :analytics_account_number => 'UA-19948002-4',
-    :analytics_domain_name    => '.dizmdayz.com').create_default_wave
+    :analytics_domain_name    => '.dizmdayz.com')
