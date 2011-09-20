7a38f72f705deeb5ff17e6f8df947a967990d5fc
Michael Bamford <michael@michaelbamford.com>
2011-09-20 23:26:02 +1000

    Added poke email image assets

--- app/models/asset/base.rb
+++ app/models/asset/base.rb
@@ -1,5 +1,7 @@
 class Asset::Base < ActiveRecord::Base
   set_table_name :assets
-  belongs_to :site
+  belongs_to :site  
+  alias_attribute :value, :asset_file_name
+  alias :to_s :value  
   scope :type, lambda { |*types| where('`assets`.`type` IN (?)', types.map(&:to_s)) }
 end
--- app/models/asset/constant.rb
+++ app/models/asset/constant.rb
@@ -1,4 +1,2 @@
 class Asset::Constant < Asset::Base
-  alias_attribute :value, :asset_file_name
-  alias :to_s :value
 end
