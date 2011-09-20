46017482ff1692a4db66f74c2bf6fe9f29fb48a2
Michael Bamford <michael@michaelbamford.com>
2011-09-21 08:10:35 +1000

    Draft of poke cocktail HTML email

--- app/models/friendship/base.rb
+++ app/models/friendship/base.rb
@@ -3,5 +3,7 @@ class Friendship::Base < ActiveRecord::Base
   belongs_to :profile, :class_name => 'Wave::Profile'
   belongs_to :friend, :class_name => 'Wave::Profile'
   validates_presence_of :profile_id, :friend_id
-  scope :type, lambda { |*types| where(:type => types.map(&:to_s)) }  
+  alias_attribute :sender, :profile
+  alias_attribute :receiver, :friend  
+  scope :type, lambda { |*types| where(:type => types.map(&:to_s)) }
 end
