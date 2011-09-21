eee40ee6459ac24973f7a181db6bb46389856f7e
Michael Bamford <michael@michaelbamford.com>
2011-09-21 11:40:08 +1000

    Cocktail poke text email

--- app/mailers/friendships_mailer.rb
+++ app/mailers/friendships_mailer.rb
@@ -1,17 +1,18 @@
 class FriendshipsMailer < ActionMailer::Base
   
-  default :from => "mailer@friskyfactory.com"
-
   def new_poke_mail(site, poke)
     return unless site.present? && poke.present?
     @site = site
     @poke = poke
-    @host = ActionMailer::Base.default_url_options[:host].gsub!('friskyfactory', @site.name)
+    original_host = ActionMailer::Base.default_url_options[:host]
+    ActionMailer::Base.default_url_options[:host].gsub!('friskyfactory', @site.name)
+    @host = ActionMailer::Base.default_url_options[:host]
     @subject = "#{@poke.profile.handle} at #{@site.display_name} has sent you a cocktail!"
     @wave_profile_url = wave_profile_url(@poke.sender)
     @unsubscribe_href = "mailto:unsubscribe@friskyfactory.com?subject=Unsubscribe from #{@site.name} cocktails&body=To #{@site.name},%0A%0APlease don't email me any more cocktails. Thanks!%0A%0AFrom #{@poke.receiver.handle}"
+    ActionMailer::Base.default_url_options[:host] = original_host
 
-    mail(:to => email_for_environment(@poke.friend), :cc => cc_for_environment(@poke.profile), :subject => @subject) do |format|
+    mail(:from => "mailer@friskyfactory.com", :to => email_for_environment(@poke.friend), :cc => cc_for_environment(@poke.profile), :subject => @subject) do |format|
       format.html { render :layout => false }
       format.text
     end
