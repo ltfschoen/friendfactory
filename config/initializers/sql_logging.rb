c9593e6f1280c0f5eccf9ab2d8707e2856954a2f
Michael Bamford <michael@michaelbamford.com>
2011-09-22 13:21:20 +1000

    Remove sql_logging initializer from staging/production

--- config/initializers/sql_logging.rb
+++ config/initializers/sql_logging.rb
@@ -1,2 +1,4 @@
-SqlLogging::Statistics.top_sql_queries = 5
-SqlLogging::Statistics.show_sql_backtrace = true
+if Rails.env.development?
+  SqlLogging::Statistics.top_sql_queries = 5
+  SqlLogging::Statistics.show_sql_backtrace = true
+end
