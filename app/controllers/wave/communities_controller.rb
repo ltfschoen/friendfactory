371940b644e490e3fc68dc84d42177b7c44ee71f
Michael Bamford <michael@michaelbamford.com>
2011-09-29 08:36:00 +1000

    Use biorealism as community layout

--- app/controllers/wave/communities_controller.rb
+++ app/controllers/wave/communities_controller.rb
@@ -7,6 +7,8 @@ class Wave::CommunitiesController < ApplicationController
   cattr_reader :per_page
   @@per_page = 50
   
+  layout 'community'
+  
   def show
     @wave = current_site.home_wave
     @postings = @wave.postings.published.includes(:user).order('sticky_until desc, updated_at desc').paginate(:page => params[:page], :per_page => @@per_page)
