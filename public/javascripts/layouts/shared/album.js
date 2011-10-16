7687db08483d524e95dcb33c5140ee6175c9230d
Michael Bamford <michael@michaelbamford.com>
2011-10-16 11:47:14 -0700

    Show newly upload album in wave

--- public/javascripts/layouts/shared/album.js
+++ public/javascripts/layouts/shared/album.js
@@ -7,35 +7,29 @@ jQuery(function($) {
 	}
 
 	$('form.new_wave_album')
-		.live('reset', function() {			
+		.live('reset', function() {
 			$(this)
 				.find('#wave_id').val('').end()
 				.find('ul.posting_photos').empty();
 		})
-		
+
 		.find('input[type="submit"]')
-			.bind('click', function(event) {			
-				var	$form = $(this).closest('form'),							
+			.bind('click', function(event) {
+				var	$form = $(this).closest('form'),
 					publishId = $form.find('#publish_id').val(),
 					albumId = $form.find('#wave_id').val();
 
 				event.preventDefault();
-		
+
 				if (albumId !== '') {
 					var url = '/waves/' + publishId + '/posting/wave_proxies',
 						stickyUntil = $form.find('#sticky_until').val();
-					
-					$form.fadeTo('fast', 0.0, function() {
-						$form.slideUp(800, 'easeOutBounce', function() {
-							$form.trigger('reset');
-						});
-					});
 
 					$.post(url, { resource_id: albumId, sticky_until: stickyUntil }, function() {}, 'script');
 				}
 			})
 		.end()
-	
+
 		.fileUploadUI({
 			dropEffect: null,
 			dropZone: $('.dropzone'),
@@ -50,37 +44,37 @@ jQuery(function($) {
 				return $('<li>' + renderPostingPhoto(photo) + '</li>');
 			},
 
-	        beforeSend: function (event, files, index, xhr, handler, callBack) {							
-	            if (index === 0) {
-	                files.uploadSequence = [];
-	                files.uploadSequence.start = function (index) {
-	                    var next = this[index];
-	                    if (next) {
-	                        next.apply(null, Array.prototype.slice.call(arguments, 1));
-	                        this[index] = null;
+	        beforeSend: function (event, files, index, xhr, handler, callBack) {
+				if (index === 0) {
+					files.uploadSequence = [];
+					files.uploadSequence.start = function (index) {
+						var next = this[index];
+						if (next) {
+							next.apply(null, Array.prototype.slice.call(arguments, 1));
+							this[index] = null;
 						}
-	                };
-	            }
+					};
+				}
 
 				(pulseCycle = function() {
 					$('.no-cssanimations .new_wave_album ul.posting_photos .photo.pulse')
 						.animate({ opacity: 0.6 }, 300)
 						.animate({ opacity: 1.0 }, 300, pulseCycle);
 				})();
-		
-	            files.uploadSequence.push(callBack);
-		
-	            if (index + 1 === files.length) {
-	                files.uploadSequence.start(0);
-	            }
-	        },
+
+				files.uploadSequence.push(callBack);
+
+				if (index + 1 === files.length) {
+					files.uploadSequence.start(0);
+				}
+			},
 
 	        onComplete: function (event, files, index, xhr, handler) {
-				var result = handler.parseResponse(xhr),					
+				var result = handler.parseResponse(xhr),
 					$form = $(handler.dropZone).closest('form');
-		
+
 				$('input#wave_id', $form).val(result.wave_id);
-		
+
 				if (result.proxy_id !== undefined) {
 					$('input#proxy_id', $form).val(result.proxy_id);
 				}
@@ -91,13 +85,12 @@ jQuery(function($) {
 						files.uploadSequence.start(i);
 					}
 				}
-	        },
+			},
 
-	        onAbort: function (event, files, index, xhr, handler) {
-	            handler.removeNode(handler.uploadRow);
-	            files.uploadSequence[index] = null;
-	            // files.uploadSequence.start(index + 1);
-	        }
+			onAbort: function (event, files, index, xhr, handler) {
+				handler.removeNode(handler.uploadRow);
+				files.uploadSequence[index] = null;
+				// files.uploadSequence.start(index + 1);
+			}
 		});
-	
 });
