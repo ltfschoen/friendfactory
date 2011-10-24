7392aa4cacd24b5fc59f8436d655f31a9ac61ccc
Michael Bamford <michael@michaelbamford.com>
2011-10-24 14:39:23 -0700

    Add video to biorealism community

--- app/controllers/posting/videos_controller.rb
+++ app/controllers/posting/videos_controller.rb
@@ -1,12 +1,14 @@
 class Posting::VideosController < Posting::BaseController
   
   def create
-    @posting = Posting::Video.new(params[:posting_video]).tap { |video| video.user = current_user }
-    if @posting.valid? && @wave = current_site.waves.find_by_id(params[:wave_id])
-      @wave.postings << @posting
-      @posting.publish!
+    @posting = nil
+    if @wave = current_site.waves.find_by_id(params[:wave_id])
+      @posting = Posting::Video.new(params[:posting_video]) { |video| video.user = current_user }
+      if @wave.postings << @posting
+        @posting.publish!
+      end
     end
-    respond_to do |format|      
+    respond_to do |format|
       format.js { render :layout => false }
     end
   end
