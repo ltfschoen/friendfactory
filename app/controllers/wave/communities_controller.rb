fe8b7b51b8869c716867cea961770140643e94fc
Michael Bamford <michael@michaelbamford.com>
2011-10-29 17:18:55 -0400

    Rollcall on community wave

--- app/controllers/wave/communities_controller.rb
+++ app/controllers/wave/communities_controller.rb
@@ -3,18 +3,34 @@ class Wave::CommunitiesController < ApplicationController
   DefaultWaveSlug = 'popular'
 
   before_filter :require_user
+
   layout 'community'
 
   cattr_reader :per_page
   @@per_page = 50
 
+  helper_method :wave, :postings, :profiles
+
   def show
-    @wave = current_site.home_wave
-    @postings = @wave.postings.published.includes(:user).order('sticky_until desc, updated_at desc').paginate(:page => params[:page], :per_page => @@per_page)
-    # @profiles = @postings.map(&:user).uniq.map{ |user| user.profile(current_site) }.uniq
-    respond_to do |format|
-      format.html
-    end
+    respond_to { |format| format.html }
+  end
+
+  def rollcall
+    respond_to { |format| format.html }
+  end
+
+  private
+
+  def wave
+    @wave ||= current_site.home_wave
+  end
+
+  def postings
+    @postings ||= wave.postings.published.includes(:user).order('sticky_until desc, updated_at desc').paginate(:page => params[:page], :per_page => @@per_page)
+  end
+
+  def profiles
+    @profiles ||= postings.map(&:user).uniq.map{ |user| user.profile(current_site) }
   end
 
 end
