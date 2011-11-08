8d9a02814a4dbd97bc4e002615d9fc5ce7093e19
Michael Bamford <michael@michaelbamford.com>
2011-11-08 11:19:54 -0700

    Hardcode no-csstransforms3d for Chrome

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,11 +1,13 @@
 (function($) {
 
-	$.support.csstransforms3d = Modernizr.csstransforms3d;
-
 	if (/chrome/.test(navigator.userAgent.toLowerCase())) {
 		$.browser.chrome = true;
 		$.browser.safari = false;
+		Modernizr.csstransforms3d = false;
+		$('html').removeClass('csstransforms3d').addClass('no-csstransforms3d');
 	}
+
+	$.support.csstransforms3d = Modernizr.csstransforms3d;
 
 	$.getId = function(element) {
 		var id = $(element).attr('id'),
--- public/javascripts/layouts/conversation.js
+++ public/javascripts/layouts/conversation.js
@@ -1,24 +1,3 @@
 jQuery(function($) {
-	var idx = 0,
-		$headshots = $('div.headshot'),
-
-		flipNext = function(headshot) {
-			headshot.onFlip = undefined;
-
-			if (idx < 3) {
-				$headshots.eq(++idx)
-					.find('.front.face a[data-pane-name="conversation"]')
-						.trigger('click');
-			} else {
-				$headshots.each(function() {
-					this.onFlip = undefined;
-				})
-			}
-		};
-
-	$headshots.headshot({ onFlip: flipNext })
-		.filter(':first')
-			.find('.front.face a[data-pane-name="conversation"]')
-				.trigger('click');
-
+	$('div.headshot').headshot({ pane: 'conversation' });
 });
