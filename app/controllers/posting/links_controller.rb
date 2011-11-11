17e7b597fa8ba9a3dd912108fd0c82b9a4d982c4
Michael Bamford <michael@michaelbamford.com>
2011-11-12 10:52:39 +1100

    Add site to link  postings

--- app/controllers/posting/links_controller.rb
+++ app/controllers/posting/links_controller.rb
@@ -6,11 +6,12 @@ class Posting::LinksController < Posting::BaseController
     @posting = nil
     if wave = current_site.waves.find_by_id(params[:wave_id])
       @posting = Posting::Link.new(params[:posting_link]) do |link|
+        link.site = current_site
         link.user = current_user
         link.sticky_until = params[:posting_link][:sticky_until] if current_user.admin?
         link.embedify
         link.build_photos
-      end      
+      end
       if wave.postings << @posting
         current_user.profile(current_site).postings << @posting
         @posting.publish!
