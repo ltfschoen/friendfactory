75955db0718603b3a9ac73bd5ae35b5524f444a7
Michael Bamford <michael@michaelbamford.com>
2011-11-12 07:06:09 +1100

    Fixed user spec

--- spec/routing/wave/profiles_routing_spec.rb
+++ spec/routing/wave/profiles_routing_spec.rb
@@ -21,15 +21,6 @@ describe Wave::ProfilesController do
       pending
       photos_waves_profile_path(42).should == '/waves/profiles/42/photos'
     end
-    
-    it 'recognizes #index' do
-      pending
-      { :get => '/rollcall' }.should route_to(:controller => 'waves/roll_calls', :action => 'index')
-    end
-
-    it 'recognizes roll_call_path' do
-      roll_call_path.should == '/rollcall'
-    end
 
     it 'recognizes #index' do
       pending
@@ -39,19 +30,7 @@ describe Wave::ProfilesController do
     it 'recognizes waves_polaroids_path' do
       pending
       waves_polaroids_path.should == '/waves/polaroids'
-    end    
+    end
   end
-
-  #    it "recognizes and generates #create" do
-  #      { :post => "/profiles" }.should route_to(:controller => "profiles", :action => "create")
-  #    end
-  #
-  #    it "recognizes and generates #update" do
-  #      { :put => "/profiles/1" }.should route_to(:controller => "profiles", :action => "update", :id => "1")
-  #    end
-  #
-  #    it "recognizes and generates #destroy" do
-  #      { :delete => "/profiles/1" }.should route_to(:controller => "profiles", :action => "destroy", :id => "1")
-  #    end
 
 end
