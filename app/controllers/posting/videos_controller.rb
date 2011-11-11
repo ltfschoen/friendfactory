059874ffacec2d872a977839a373f5361b412972
Michael Bamford <michael@michaelbamford.com>
2011-11-12 10:45:32 +1100

    Video posting published to personal wave

--- app/controllers/posting/videos_controller.rb
+++ app/controllers/posting/videos_controller.rb
@@ -3,7 +3,10 @@ class Posting::VideosController < Posting::BaseController
   def create
     @posting = nil
     if @wave = current_site.waves.find_by_id(params[:wave_id])
-      @posting = Posting::Video.new(params[:posting_video]) { |video| video.user = current_user }
+      @posting = Posting::Video.new(params[:posting_video]) do |video|
+        video.site = current_site
+        video.user = current_user
+      end
       if @wave.postings << @posting
         @posting.publish!
       end
