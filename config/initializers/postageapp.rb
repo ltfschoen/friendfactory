b20cf1bd1aa1d2ff96c3e6e9e7d206958031e49d
Michael Bamford <michael@michaelbamford.com>
2011-11-13 05:32:15 +1100

    Mailer host on staging

--- config/initializers/postageapp.rb
+++
@@ -1,4 +0,0 @@
-# PostageApp.configure do |config|
-#   config.api_key = 'sjon0AyJjuAa1bDzRSbv6HtcyAJHZd6V'
-#   config.recipient_override = 'michael@michaelbamford.com' unless Rails.env.production?
-# end
