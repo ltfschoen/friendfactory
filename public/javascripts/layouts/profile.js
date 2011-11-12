dee14072ec5e4f8147d02953d3db2a71623d1959
Michael Bamford <michael@michaelbamford.com>
2011-11-12 12:44:45 +1100

    Placeholder on personal wave and profile edit

--- public/javascripts/layouts/profile.js
+++ public/javascripts/layouts/profile.js
@@ -1,12 +1,16 @@
 jQuery(function($) {
-	
+
 	// Personal Wave
 	$('.profiles.show')
 		.find('.headshot-container').headshot();
-	
+
+	// Initialize forms
+	$('input[type="text"], textarea', 'form').placeholder();
+
 	// Edit profile
 	$('input[name="user[emailable]"]').bind('ajax:before', function() {
 		$(this).data('params', this.name + '=' + this.checked);
 		return true;
 	});
+
 });
