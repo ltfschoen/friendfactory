0877b685cffe4820efcfd855a2286046992411b6
Michael Bamford <michael@michaelbamford.com>
2011-11-12 11:28:42 +1100

    Add posting to home wave from profile wave

--- app/models/wave/profile.rb
+++ app/models/wave/profile.rb
@@ -123,6 +123,11 @@ class Wave::Profile < Wave::Base
     self.has_friended?(profile_id, ::Friendship::Poke)
   end
 
+  def add_posting_to_other_waves(posting)
+    add_posting_to_home_wave(posting)
+    super
+  end
+
   private
 
   def touch(avatar = nil)
