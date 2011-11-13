91d517264d6e13f395ed739dbe8810804ab11e73
Michael Bamford <michael@michaelbamford.com>
2011-11-14 06:50:51 +1100

    Refactored invitations mailer

--- app/mailers/invitations_mailer.rb
+++ app/mailers/invitations_mailer.rb
@@ -1,19 +1,14 @@
-class InvitationsMailer < ActionMailer::Base
+class InvitationsMailer < ApplicationMailer
   
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
-  #
   #   en.invitations.invitation.subject
-  #
-
-  default :from => "mailer@friskyfactory.com"
 
   def new_invitation_mail(invitation)
     @invitation = invitation
-    @host = ActionMailer::Base.default_url_options[:host].gsub('friskyfactory', @invitation.site.name)
-    email = Rails.env.production? ? @invitation.email : 'michael@michaelbamford.com'
+    @host = host_for_site(@invitation.site)
     subject = "Invitation from #{@invitation.sponsor.handle(@invitation.site)} to join #{@invitation.site.display_name}!"
-    mail :to => email, :subject => subject
+    mail :from => @invitation.site.mailer, :to => email_for_environment(@invitation.email), :subject => subject
   end
   
 end
