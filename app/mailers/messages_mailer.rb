29a79947bbd1ca59155a41069bb8360c7b7d7f08
Michael Bamford <michael@michaelbamford.com>
2011-11-14 06:57:52 +1100

    Refactored messages mailer

--- app/mailers/messages_mailer.rb
+++ app/mailers/messages_mailer.rb
@@ -1,13 +1,10 @@
-class MessagesMailer < ActionMailer::Base
-
-  default :from => "mailer@friskyfactory.com"
+class MessagesMailer < ApplicationMailer
 
   def new_message_notification(message)
-    @message = message
-    @host = ActionMailer::Base.default_url_options[:host].gsub('friskyfactory', @message.site.name)    
-    email = Rails.env.production? ? @message.receiver.email : 'michael@michaelbamford.com'
-    subject = "Message from #{@message.sender.handle(@message.site)} at #{@message.site.display_name}"
-    mail :to => email, :subject => subject
+    @message, @site = message, message.site
+    @host = host_for_site(@site)
+    subject = "Message from #{@message.sender.handle(@message.site)} at #{@site.display_name}"
+    mail :from => @site.mailer, :to => email_for_environment(@message.receiver.email), :subject => subject
   end
 
 end
