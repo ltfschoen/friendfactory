b0054b9f7493021412af3e397bebdae05c430b82
Michael Bamford <michael@michaelbamford.com>
2011-11-15 13:35:37 +1100

    Simplified landing page. Fixed squished problem

--- public/javascripts/layouts/welcome.js
+++ public/javascripts/layouts/welcome.js
@@ -1,8 +1,24 @@
 jQuery(function($) {
+
+  	// setInterval(function(){
+  	// 	var obj = $('#stream .portrait').first()
+  	// 	obj.clone().appendTo('#container');
+  	// 	obj.animate({marginLeft: -35}, 500, function(){
+  	// 		obj.remove();
+  	// 	});
+  	// }, 2500);
+
 	$('div.headshot').headshot({ panes: { conversation: function(){} }});
+
 	$('a.flip.icon[title]')
 		.tooltip()
 		.click(function() {
 			$(this).data('tooltip').getTip().hide();
 		});
+});
+
+jQuery(window).load(function() {
+	setInterval(function(){
+	  $('#badge').addClass('animation');
+	}, 900);    
 });
