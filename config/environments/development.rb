58e1b99476972bb23b96905f3e1d2d42fb065967
Michael Bamford <michael@michaelbamford.com>
2011-11-16 21:46:20 +1100

    Added letter_opener to developent environment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,7 +28,7 @@ Friskyfactory::Application.configure do
 
   config.action_mailer.delivery_method = :smtp
   # config.action_mailer.delivery_method = :letter_opener
-  # config.action_mailer.delivery_method = :test
+  config.action_mailer.delivery_method = :test
 
   config.action_mailer.smtp_settings = {
       :address              => 'smtp.postmarkapp.com',
