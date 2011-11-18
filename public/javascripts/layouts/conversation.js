959a70ee44d13e0071bba604d23b1dcd3fe39f17
Michael Bamford <michael@michaelbamford.com>
2011-11-18 16:21:38 +1100

    Move conversation to page load from window load

--- public/javascripts/layouts/conversation.js
+++ public/javascripts/layouts/conversation.js
@@ -1,11 +1,13 @@
-jQuery(window).load(function() {
-	$('div.headshot').headshot({ pane: 'conversation', setFocus: false });
-	
+jQuery(function($) {
 	$('a.conversations').bind('ajax:success', function (data, status) {
 		var $this = $(this).closest('.datecell'),
 			idx = $this.index();
-			
+
 		$this.closest('.datecell').replaceWith(status);
 		$('.datecell').eq(idx).find('div.headshot').headshot({ pane: 'conversation', setFocus: false });
 	});
+});
+
+jQuery(window).load(function() {
+	$('div.headshot').headshot({ pane: 'conversation', setFocus: false });
 });
