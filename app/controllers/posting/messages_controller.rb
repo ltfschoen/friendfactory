c7f7dee6325165279b0a30e712a79a7457b757e0
Michael Bamford <michael@michaelbamford.com>
2011-11-21 08:21:45 +1100

    Refactory sending messages

--- app/controllers/posting/messages_controller.rb
+++ app/controllers/posting/messages_controller.rb
@@ -14,35 +14,51 @@ class Posting::MessagesController < ApplicationController
   end
 
   def create
-    if @wave = current_user.conversations.find_by_id(params[:wave_id])
-      @posting = Posting::Message.new(params[:posting_message]) do |posting|
-        posting.sender = current_user
-        posting.receiver = @wave.recipient
-        posting.site = current_site
-      end
-      if @posting.save
-        @posting.publish!
-        @wave.postings << @posting
-        @wave.read!
-        broadcast_posting(@posting, (@posting.waves(true) - [ @wave ]))
-      end
-    end
     respond_to do |format|
-      format.js { render :layout => false }
+      success = false
+      if @wave = current_user.conversations.find_by_id(params[:wave_id])
+        @posting = Posting::Message.new(params[:posting_message]) { |posting| initialize_message(posting, @wave.recipient) }
+        if success = @posting.save && add_message_to_conversation(@posting, @wave)
+          conversations = @posting.waves(true).except(@wave)
+          broadcast_posting(@posting, conversations)
+        end
+      end
+      format.json { render :json => { :success => success }}
     end
   end
 
   private
 
+  def initialize_message(posting, recipient)
+    posting.site = current_site
+    posting.sender = current_user
+    posting.receiver = recipient
+  end
+
+  def add_message_to_conversation(posting, wave)
+    wave.postings << posting
+    wave.read! && posting.publish!
+  rescue
+    false
+  end
+
   def broadcast_posting(posting, waves)
+    broadcast_posting_via_pusher(posting, waves)
+    broadcast_posting_via_email(posting)
+  end
+
+  def broadcast_posting_via_pusher(posting, waves)
     waves.each do |wave|
       channel_id = dom_id(wave)
       Pusher[channel_id].trigger('message', { :url => wave_posting_message_path(wave, posting), :dom_id => "##{channel_id}" })
-    end    
+    end
+  end
+
+  def broadcast_posting_via_email(posting)
     receiver = posting.receiver
     if !receiver.online? && receiver.emailable?
       MessagesMailer.new_message_notification(posting).deliver
-    end    
+    end
   end
-  
+
 end
--- app/models/posting/base.rb
+++ app/models/posting/base.rb
@@ -33,7 +33,11 @@ class Posting::Base < ActiveRecord::Base
 
   has_many :children, :class_name  => 'Posting::Base', :foreign_key => 'parent_id'
   has_many :publications, :as => :resource
-  has_many :waves, :through => :publications, :order => 'updated_at desc'
+  has_many :waves, :through => :publications, :order => 'updated_at desc' do
+    def except(*waves)
+      where('`waves`.`id` not in (?)', waves.map(&:id))
+    end
+  end
 
   def attributes=(attrs)
     sanitize_for_mass_assignment(attrs).each do |k, v|
