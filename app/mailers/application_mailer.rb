e73396b9f2116a9220149fc64197f9d13bb2bfca
Michael Bamford <michael@michaelbamford.com>
2011-11-21 06:58:49 +1100

    Turned of bcc for ml, mjb

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -2,12 +2,23 @@ class ApplicationMailer < ActionMailer::Base
 
   default :from => Site::DefaultMailer
 
+  BamfordId = 1
+  LyonId = 2
+
+  private
+
   def email_for_environment(email)
     [ 'staging', 'production' ].include?(Rails.env) ? email : 'michael@michaelbamford.com'
   end
 
   def host_for_site(site)
     ActionMailer::Base.default_url_options[:host].gsub('friskyfactory', site.name)
+  end
+
+  def bcc_for_environment(message)
+    if [ 'staging', 'production' ].include?(Rails.env) && [ BamfordId, LyonId ].include?(message.sender_id)
+      message.sender.email
+    end
   end
 
 end
