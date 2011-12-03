0b4e14dac6480d5d42ca38987ef1d48e93b6ed0f
Michael Bamford <michael@michaelbamford.com>
2011-12-03 20:06:26 +1100

    Personals controller spec

--- spec/controllers/admin/invitation/personals_controller_spec.rb
+++ spec/controllers/admin/invitation/personals_controller_spec.rb
@@ -1,31 +1,53 @@
 require 'spec_helper'
 
-describe Admin::Invitation::PersonalsController do
-  
-  fixtures :sites, :users
-  
-  describe "requires an administrator" do
-    it "to access" do
-      login(:positivelyfrisky, :adam)
-      response.should be_successful
-    end
-    
-    it "otherwise disallows" do
+describe Admin::Invitation::PersonalsController, 'GET index' do
+
+  def not_logged_in
+    controller.stub!(:current_user).and_return(nil)
+  end
+
+  def login_as_user
+    controller.stub!(:current_user).and_return(mock_model(User, :admin? => false))
+  end
+
+  def login_as_admin
+    controller.stub!(:current_user).and_return(mock_model(User, :admin? => true))
+  end
+
+  context 'when user not logged in' do
+    before(:each) { not_logged_in }
+    it 'redirects to welcome page' do
       get :index
       response.should redirect_to(welcome_url)
     end
   end
 
-  describe 'GET index' do    
-    let(:wave) { mock_model(Wave::Invitation).as_null_object }
-        
-    before(:each) { login(:friskyhands, :adam) }
+  context 'when user logged in' do
+    before(:each) { login_as_user }
+    it 'redirects to welcome page' do
+      get :index
+      response.should redirect_to(welcome_url)
+    end
+  end
 
-    it "assigns invitation waves to @waves" do
-      Wave::Invitation.should_receive(:find_all_by_site_and_fully_offered).and_return([ wave ])
+  context 'when administrator logged in' do
+    let(:wave) do
+      mock_model(Wave::Invitation).as_null_object
+    end
+
+    before(:each) do
+      login_as_admin
+      Wave::Invitation.should_receive(:find_all_by_site_and_fully_offered).and_return([ wave ])      
+    end
+
+    it 'assigns @waves' do
       get :index
       assigns(:waves).should eq([ wave ])
     end
+
+    it 'renders index' do
+      get :index
+      response.should render_template('index')
+    end
   end
-  
 end
