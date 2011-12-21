00252725f64fbfd4f95cb795a61721215bf51019
Michael Bamford <michael@michaelbamford.com>
2011-12-21 19:24:22 +1100

    Permitted to show nav

--- app/models/wave/ambassador.rb
+++ app/models/wave/ambassador.rb
@@ -1,6 +1,13 @@
 class Wave::Ambassador < Wave::Profile
+
   def technical_description
     [ super, slug, person.handle ].compact * ' - '
   end
-  
+
+  private
+
+  def permitted_user_ids
+    self[:user_id]
+  end
+
 end
