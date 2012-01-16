22d0a875ef31d25d3209f9903da749c41dc00a3e
Michael Bamford <michael@michaelbamford.com>
2012-01-16 20:54:01 +1100

    Ambassador persona

--- app/models/persona/ambassador.rb
+++ app/models/persona/ambassador.rb
@@ -1,3 +1,9 @@
 class Persona::Ambassador < Persona::Person
+
   self.default_profile_type = 'Wave::Ambassador'
+
+  attr_accessible :location, :description
+
+  validates_presence_of :handle, :location, :description
+
 end
