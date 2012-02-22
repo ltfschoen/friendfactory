f2af1c484ed10d190db10625b8d028226e2fe07a
Michael Bamford <michael@michaelbamford.com>
2012-02-22 14:54:08 +1100

    Removed obsolete posting js

--- public/javascripts/app/posting/avatar.js
+++
@@ -1,5 +0,0 @@
-jQuery(function($) {
-	$('.posting_avatar').live('init', function(event) {
-		$('div.polaroid', this).polaroid();
-	});
-});
--- public/javascripts/app/posting/event.js
+++
@@ -1,7 +0,0 @@
-jQuery(function($) {
-
-	$('.posting_event').live('init', function(event) {
-		$('.ticket-container > .ticket', this).ticket();
-	});
-
-});
