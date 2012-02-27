1ec76e450565dc25ddddbd0a288da1457fbd02b3
Michael Bamford <michael@michaelbamford.com>
2012-02-27 21:02:03 +1100

    Refactor ambassador edit profile to use avatar upload

--- app/models/persona/ambassador.rb
+++ app/models/persona/ambassador.rb
@@ -1,9 +1,25 @@
-class Persona::Ambassador < Persona::Person
+require 'geolocation'
+
+class Persona::Ambassador < Persona::Base
+
+  include Geolocation
 
   self.default_profile_type = 'Wave::Ambassador'
 
   attr_accessible :location, :description
 
-  validates_presence_of :handle, :location
+  validates_presence_of :handle
+
+  def first_name
+    self[:first_name].present? ? self[:first_name].strip.titleize : handle
+  end
+
+  def last_name
+    self[:last_name].strip.titleize if self[:last_name].present?
+  end
+
+  def full_name
+    [ first_name, last_name ].compact.join(' ')
+  end
 
 end
