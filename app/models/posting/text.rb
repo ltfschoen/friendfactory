6b257fc32206d9704fcd6a9816f8839ddd661e00
Michael Bamford <michael@michaelbamford.com>
2012-02-29 13:09:40 +1100

    Sanitize text postings

--- app/models/posting/text.rb
+++ app/models/posting/text.rb
@@ -1,6 +1,20 @@
 class Posting::Text < Posting::Base
+
   def body=(text)
-    self[:body] = text
-    self[:subject] = BlueCloth.new(text).to_html
+    sanitize_text = tag_helper.sanitize(text)
+    self[:body] = sanitize_text
+    self[:subject] = sanitize_text.blank? ? '' : BlueCloth.new(sanitize_text).to_html
   end
+
+  private
+
+  def tag_helper
+    TagHelper.instance
+  end
+
+  class TagHelper
+    include Singleton
+    include ActionView::Helpers::SanitizeHelper
+  end
+
 end
