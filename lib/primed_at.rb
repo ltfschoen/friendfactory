fb174c58a2fd3fe029f9c7297853cc6a6f88ece8
Michael Bamford <michael@michaelbamford.com>
2012-03-23 10:23:56 +1100

    primed_at takes value of commented_at

--- lib/primed_at.rb
+++ lib/primed_at.rb
@@ -1,13 +1,13 @@
 module PrimedAt
 
-  def update_primed_at
-    self[:primed_at] = self[:updated_at]
-    self.class.update_all({ :primed_at => self[:updated_at]}, self.class.primary_key => self[:id])
-  end
-
   def touch(attribute = nil)
     super
-    update_primed_at
+    update_primed_at(self[:commented_at]) if attribute.to_sym == :commented_at
+  end
+
+  def update_primed_at(datetime)
+    self[:primed_at] = datetime
+    self.class.update_all({ :primed_at => datetime}, self.class.primary_key => self[:id])
   end
 
 end
