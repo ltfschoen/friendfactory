05b2a2bd6658576adb3ccbffd1487e19e31b2a23
Michael Bamford <michael@michaelbamford.com>
2012-03-26 16:45:47 +1100

    STI initializer required for environments other than development

--- config/initializers/sti.rb
+++ config/initializers/sti.rb
@@ -1,5 +1,3 @@
-if Rails.env.development?
-  Dir[File.join(Rails.root, 'app', 'models', '{posting,wave}', '*.rb')].each do |file|
-    require_dependency(file)
-  end
+Dir[File.join(Rails.root, 'app', 'models', '{posting,wave}', '*.rb')].each do |file|
+  require_dependency(file)
 end
