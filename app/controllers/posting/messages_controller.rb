63fb9da395541019390d0843f79f4b2956a16a60
Michael Bamford <michael@michaelbamford.com>
2012-04-30 20:31:01 +1000

    after_filter for notify messages

--- app/controllers/posting/messages_controller.rb
+++ app/controllers/posting/messages_controller.rb
@@ -4,6 +4,8 @@ class Posting::MessagesController < ApplicationController
 
   helper_method :wave, :posting
 
+  after_filter :notify, :only => [ :create ]
+
   def show
     respond_to do |format|
       wave.read!
@@ -15,7 +17,6 @@ class Posting::MessagesController < ApplicationController
     respond_to do |format|
       if wave.postings << new_message
         wave.mark_as_read
-        notify_subscribers
         format.json { render :json => { :success => true }}
       else
         format.json { head :unprocessable_entity }
@@ -45,9 +46,11 @@ class Posting::MessagesController < ApplicationController
 
   ###
 
-  def notify_subscribers
-    notify_via_pusher
-    notify_via_email
+  def notify
+    if new_message.persisted?
+      notify_via_pusher
+      notify_via_mailer
+    end
   end
 
   def notify_via_pusher
@@ -56,7 +59,7 @@ class Posting::MessagesController < ApplicationController
     Pusher[channel_id].trigger('message', { :url => wave_posting_message_path(wave, new_message), :dom_id => "##{channel_id}" })
   end
 
-  def notify_via_email
+  def notify_via_mailer
     new_message.subscriptions.notify do |subscriber|
       Posting::MessagesMailer.delay.create(subscriber, new_message, current_site, request.host, request.port)
     end
