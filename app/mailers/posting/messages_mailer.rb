c8b3d8c9eb43f6499fb531d8041eb3802bfb5d01
Michael Bamford <michael@michaelbamford.com>
2012-04-30 12:24:13 +1000

    Refactored site_mailer to application mailer

--- app/mailers/posting/messages_mailer.rb
+++ app/mailers/posting/messages_mailer.rb
@@ -4,23 +4,17 @@ class Posting::MessagesMailer < ApplicationMailer
 
   def create(recipient, message, site, host, port)
     super
-    mail :from => sender_email, :to => recipient_email, :subject => subject
+    mail :from => site_mailer, :to => recipient_email, :subject => subject
   end
 
   private
-
-  def sender_email
-    site.mailer
-  end
 
   def recipient_email
     email_for_environment(posting.receiver.email, posting.sender.email)
   end
 
   def subject
-    @subject ||= begin
-      "Message from #{posting.sender.display_handle} at #{site.display_name}"
-    end
+    "Message from #{posting.sender.display_handle} at #{site.display_name}"
   end
 
 end
