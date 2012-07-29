267313e90a1420c3db557cba094a707fad1e8d90
Michael Bamford <michael@michaelbamford.com>
2012-07-29 17:43:31 +1000

    Persona placeholder spec

---
+++ spec/models/persona/base_spec.rb
@@ -0,0 +1,9 @@
+# it 'emailable by default' do
+#   User.new(attrs).should be_emailable
+# end
+# 
+# it 'is not emailable' do
+#   user = User.new(attrs)
+#   user.emailable = false
+#   user.should_not be_emailable
+# end
