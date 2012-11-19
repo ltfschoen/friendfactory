bace653b16eb79599afbf7832da8c161d8bf3b8a
Michael Bamford <michael@michaelbamford.com>
2012-11-20 07:58:24 +1100

    Welcome layout with assets

--- app/assets/javascripts/development.js
+++ app/assets/javascripts/development.js
@@ -1,3 +1,7 @@
+//= require jquery-1.6.4
+//= require jquery.tools-1.2.6.tiny.min
+//= require_self
+
 (function($) {
 	// Modernizr.csstransforms3d = false;
 	// $('html').removeClass('csstransforms3d');
