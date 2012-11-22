50fa6255f3fa74acc7dfa7798dcb3f16f35d81ae
Michael Bamford <michael@michaelbamford.com>
2012-11-23 07:30:35 +1100

    Added rollcall to assets pipeline

---
+++ app/assets/javascripts/layouts/rollcall/index.js
@@ -0,0 +1,12 @@
+//= require modernizr
+//= require jquery-ui-1.8.16.custom.min
+//= require jquery.flip
+//= require jquery.scrollTo-1.4.2-min
+//= require jquery.placeholder.min
+//= require jquery.chat
+//= require jquery.headshot
+//= require jquery_ujs
+//= require application
+//= require layouts/shared/compatibility
+//= require layouts/shared/sidebar
+//= require_tree .
