53525026a2aec80c6311e242140f769ee104d31b
Michael Bamford <michael@michaelbamford.com>
2012-11-23 06:27:44 +1100

    Added inbox layout to asset pipeline

--- app/assets/javascripts/layouts/inbox/index.js
+++ app/assets/javascripts/layouts/inbox/index.js
@@ -1,3 +1,15 @@
+//= require modernizr
+//= require jquery-ui-1.8.16.custom.min
+//= require jquery.flip
+//= require jquery.scrollTo-1.4.2-min
+//= require jquery.chat
+//= require jquery.headshot
+//= require jquery.effects
+//= require jquery_ujs
+//= require application
+//= require layouts/shared/sidebar
+//= require_self
+
 jQuery(window).load(function() {
 
 	var callback = function ($headshot) {
