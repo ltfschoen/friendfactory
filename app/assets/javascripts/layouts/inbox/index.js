dfbac54049db18fc2cdf5dcb27ffd4f9eefc383b
Michael Bamford <michael@michaelbamford.com>
2012-12-01 10:42:59 +1100

    Fixed inbox page assets

--- app/assets/javascripts/layouts/inbox/index.js
+++ app/assets/javascripts/layouts/inbox/index.js
@@ -1,5 +1,5 @@
 //= require modernizr
-//= require jquery-ui-1.8.16.custom.min
+//= require jquery-ui/1.8.16/jquery-ui.custom.min
 //= require jquery.flip
 //= require jquery.scrollTo-1.4.2-min
 //= require jquery.chat.js
